e743083e595ffea45eab41834410ae44459975d6
ianwookim <ianwookim@gmail.com>
2011-12-16 12:02:28 +0000

    clean up unnecessary files and copied Generic.hs from xournal-render package

--- lib/Graphics/Xournal/Type/Command.hs
+++
@@ -1,9 +0,0 @@
-module Graphics.Xournal.Type.Command where
-
-import Graphics.Xournal.Type.Type
-import Graphics.Xournal.Type.Job
-
-commandLineProcess :: Xournal_types -> IO ()
-commandLineProcess Test = do 
-  putStrLn "test called"
-  startJob
--- lib/Graphics/Xournal/Type/Job.hs
+++
@@ -1,5 +0,0 @@
-module Graphics.Xournal.Type.Job where
-
-startJob :: IO () 
-startJob = do 
-  putStrLn "job started"

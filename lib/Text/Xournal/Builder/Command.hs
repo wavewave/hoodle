04ac2e3a8da50f0fd81841bc5a984c595554547d
ianwookim <ianwookim@gmail.com>
2011-12-17 02:29:54 +0000

    first compilable successful builder refactored from hxournal

--- lib/Text/Xournal/Builder/Command.hs
+++
@@ -1,9 +0,0 @@
-module Text.Xournal.Builder.Command where
-
-import Text.Xournal.Builder.Type
-import Text.Xournal.Builder.Job
-
-commandLineProcess :: Xournal_builder -> IO ()
-commandLineProcess Test = do 
-  putStrLn "test called"
-  startJob
--- lib/Text/Xournal/Builder/Job.hs
+++
@@ -1,5 +0,0 @@
-module Text.Xournal.Builder.Job where
-
-startJob :: IO () 
-startJob = do 
-  putStrLn "job started"

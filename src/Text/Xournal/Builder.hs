1de17138b90c0b47a755797f1355e64a7b90ca29
ianwookim <ianwookim@gmail.com>
2012-01-18 01:49:41 +0000

    add all module headers

--- src/Text/Xournal/Builder.hs
+++ src/Text/Xournal/Builder.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Text.Xournal.Builder 
+-- Copyright   : (c) 2011, 2012 Ian-Woo Kim
+--
+-- License     : BSD3
+-- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
+-- Stability   : experimental
+-- Portability : GHC
+--
+
 module Text.Xournal.Builder where
 
 import Data.Xournal.Simple

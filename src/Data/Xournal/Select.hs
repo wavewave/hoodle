ef23f1c7acff99091a0cfab9758d2e9c8768c130
Ian-Woo Kim <ianwookim@gmail.com>
2012-09-03 01:10:45 -0400

    change fclabels to lens

--- src/Data/Xournal/Select.hs
+++ src/Data/Xournal/Select.hs
@@ -12,6 +12,8 @@
 -- Stability   : experimental
 -- Portability : GHC
 --
+-- representing selection of xournal type 
+-- 
 -----------------------------------------------------------------------------
 
 module Data.Xournal.Select where
@@ -19,37 +21,48 @@ module Data.Xournal.Select where
 import Control.Applicative hiding (empty)
 import Control.Compose
 import Data.Foldable
-import Data.Traversable
 import Data.Monoid
 import Data.Sequence
+import Data.Traversable
+-- from this package
 import Data.Xournal.Generic
-
+-- 
 import Prelude hiding (zipWith, length, splitAt)
 
+-- |
 newtype SeqZipper a = SZ { unSZ :: (a, (Seq a,Seq a)) } 
 
+-- |
 deriving instance Functor SeqZipper
+
+-- |
 deriving instance Foldable SeqZipper 
 
+-- |
 instance Applicative SeqZipper where
   pure = singletonSZ 
   SZ (f,(f1s,f2s)) <*> SZ (x,(y1s,y2s)) = SZ (f x, (zipWith id f1s y1s, zipWith id f2s y2s))
 
+-- |
 deriving instance Traversable SeqZipper 
 
+-- |
 singletonSZ :: a -> SeqZipper a  
 singletonSZ x = SZ (x, (empty,empty))
 
+-- |
 lengthSZ :: SeqZipper a -> Int 
 lengthSZ (SZ (_x, (x1s,x2s))) = length x1s + length x2s + 1 
 
-
+-- |
 currIndex :: SeqZipper a -> Int
 currIndex (SZ (_x, (x1s,_x2s))) = length x1s 
 
+-- |
 appendGoLast :: SeqZipper a -> a -> SeqZipper a
 appendGoLast (SZ (y,(y1s,y2s))) x = SZ (x, ((y1s |> y) >< y2s, empty))
 
+-- |
 chopFirst :: SeqZipper a -> Maybe (SeqZipper a)
 chopFirst (SZ (y,(y1s,y2s))) = 
   case viewl y1s of
@@ -58,60 +71,67 @@ chopFirst (SZ (y,(y1s,y2s))) =
                 z :< zs -> Just (SZ (z,(empty,zs)))
     _z :< zs -> Just (SZ (y,(zs,y2s)))
     
+-- | 
 moveLeft :: SeqZipper a -> Maybe (SeqZipper a)
 moveLeft (SZ (x,(x1s,x2s))) = 
   case viewr x1s of
     EmptyR -> Nothing 
     zs :> z -> Just (SZ (z,(zs,x<|x2s)))
 
+-- |
 moveRight :: SeqZipper a -> Maybe (SeqZipper a) 
 moveRight (SZ (x,(x1s,x2s))) = 
   case viewl x2s of 
     EmptyL -> Nothing
     z :< zs -> Just (SZ (z,(x1s|>x,zs)))
 
+-- |
 moveTo :: Int -> SeqZipper a -> Maybe (SeqZipper a) 
 moveTo n orig@(SZ (x,(x1s,x2s))) = 
   let n_x1s = length x1s 
       n_x2s = length x2s 
-      result | n < 0 || n > n_x1s + n_x2s = Nothing 
-             | n == n_x1s = Just orig 
-             | n < n_x1s = let (x1s1, x1s2) = splitAt n x1s 
-                               el :< rm = viewl x1s2
-                           in Just (SZ (el, (x1s1,(rm |> x) >< x2s)))
-             | n > n_x1s = let (x2s1,x2s2) = splitAt (n-n_x1s-1) x2s
-                               el :< rm = viewl x2s2
-                           in Just (SZ (el, ((x1s |> x) >< x2s1, rm)))
-             | otherwise = error "error in moveTo"
-  in result 
+      res | n < 0 || n > n_x1s + n_x2s = Nothing 
+          | n == n_x1s = Just orig 
+          | n < n_x1s = let (x1s1, x1s2) = splitAt n x1s 
+                            el :< rm = viewl x1s2
+                        in Just (SZ (el, (x1s1,(rm |> x) >< x2s)))
+          | n > n_x1s = let (x2s1,x2s2) = splitAt (n-n_x1s-1) x2s
+                            el :< rm = viewl x2s2
+                        in Just (SZ (el, ((x1s |> x) >< x2s1, rm)))
+          | otherwise = error "error in moveTo"
+  in res 
 
+-- | 
 goFirst :: SeqZipper a -> SeqZipper a 
 goFirst orig@(SZ (x,(x1s,x2s))) =
   case viewl x1s of 
     EmptyL -> orig
     z :< zs -> SZ (z,(empty, zs `mappend` (x <| x2s)))  
 
+-- |
 goLast :: SeqZipper a -> SeqZipper a 
 goLast orig@(SZ (x,(x1s,x2s))) = 
   case viewr x2s of 
     EmptyR -> orig
     zs :> z -> SZ (z,((x1s |> x) `mappend` zs , empty))
  
-
-
+-- | 
 current :: SeqZipper a -> a 
 current (SZ (x,(_,_))) = x
 
+-- | 
 prev :: SeqZipper a -> Maybe a 
 prev = fmap current . moveLeft
 
+-- |
 next :: SeqZipper a -> Maybe a 
 next = fmap current . moveRight
 
+-- |
 replace :: a -> SeqZipper a -> SeqZipper a 
 replace y (SZ (_x,zs)) = SZ (y,zs)
 
-
+-- |
 deleteCurrent :: SeqZipper a -> Maybe (SeqZipper a)
 deleteCurrent (SZ (_,(xs,ys))) = 
   case viewl ys of 
@@ -120,36 +140,37 @@ deleteCurrent (SZ (_,(xs,ys))) =
                 zs :> z -> Just (SZ (z,(zs,ys)))
     z :< zs -> Just (SZ (z,(xs,zs)))
 
-
+-- |
 data ZipperSelect a = NoSelect { allelems :: [a] }  
                     | Select { zipper :: (Maybe :. SeqZipper) a }
 
-
+-- | 
 deriving instance Functor ZipperSelect 
 
+-- |
 selectFirst :: ZipperSelect a -> ZipperSelect a 
 selectFirst (NoSelect []) = NoSelect []
 selectFirst (NoSelect lst@(_:_))  = Select . gFromList $ lst
 selectFirst (Select (O Nothing)) = NoSelect []
 selectFirst (Select (O msz)) = Select . O $ return . goFirst =<<  msz
 
+-- |
 instance GListable (Maybe :. SeqZipper) where
   gFromList [] = O Nothing 
   gFromList (x:xs) = O (Just (SZ (x, (empty,fromList xs))))
   gToList (O Nothing) = [] 
   gToList (O (Just (SZ (x,(xs,ys))))) = toList xs ++ (x : toList ys)
 
+-- |
 instance GListable ZipperSelect where
   gFromList xs = NoSelect xs
   gToList (NoSelect xs) = xs 
   gToList (Select xs) = gToList xs
 
-
+-- | 
 deriving instance Foldable ZipperSelect
 
+-- |
 deriving instance Traversable ZipperSelect
 
---- 
-
--- data HeteroSeqZipper a b = HSZ { unHSZ :: (b, (Seq a, Seq a)) } 
 
--- src/Data/Xournal/Simple.hs
+++ src/Data/Xournal/Simple.hs
@@ -15,21 +15,23 @@
 
 module Data.Xournal.Simple where
 
-import Control.Applicative 
+-- from other packages
+import           Control.Applicative 
+import           Control.Lens 
 import qualified Data.ByteString as S
-import Data.ByteString.Char8 hiding (map)
-import Data.Strict.Tuple
-import Data.Xournal.Util
-import Data.Label
-import Prelude hiding ((.),id,putStrLn,fst,snd,curry,uncurry)
+import           Data.ByteString.Char8 hiding (map)
+-- import           Data.Label
 import qualified Data.Serialize as SE
+import           Data.Strict.Tuple
+-- from this package
+import           Data.Xournal.Util
+-- 
+import Prelude hiding ((.),id,putStrLn,fst,snd,curry,uncurry)
 
 -- | 
-
 type Title = S.ByteString
 
 -- | 
-
 data Stroke = Stroke { stroke_tool  :: !S.ByteString
                      , stroke_color :: !S.ByteString
                      , stroke_width :: !Double
@@ -42,7 +44,6 @@ data Stroke = Stroke { stroke_tool  :: !S.ByteString
             deriving (Show,Eq,Ord)
 
 -- | 
-
 instance SE.Serialize Stroke where
     put Stroke{..} = SE.putWord8 0 
                      >> SE.put stroke_tool 
@@ -57,9 +58,8 @@ instance SE.Serialize Stroke where
              case tag of 
                0 -> Stroke <$> SE.get <*> SE.get <*> SE.get <*> SE.get
                1 -> VWStroke <$> SE.get <*> SE.get <*> SE.get
-  
+               _ -> fail "err in Stroke parsing"
 -- |    
-               
 instance (SE.Serialize a, SE.Serialize b) => SE.Serialize (Pair a b) where
     put (x :!: y) = SE.put x
                     >> SE.put y
@@ -67,12 +67,10 @@ instance (SE.Serialize a, SE.Serialize b) => SE.Serialize (Pair a b) where
 
     
 -- | 
-
 data Dimension = Dim { dim_width :: !Double, dim_height :: !Double }
                deriving Show
 
 -- | 
-
 data Background = Background { bkg_type :: !S.ByteString 
                              , bkg_color :: !S.ByteString 
                              , bkg_style :: !S.ByteString 
@@ -85,93 +83,99 @@ data Background = Background { bkg_type :: !S.ByteString
                 deriving Show 
 
 -- | 
-
 data Xournal = Xournal { xoj_title :: !Title, xoj_pages :: ![Page] }
              deriving Show 
 
 -- | 
-
 data Page = Page { page_dim :: !Dimension
                  , page_bkg :: !Background 
                  , page_layers :: ![Layer] }
           deriving Show 
 
 -- | 
-
 data Layer = Layer { layer_strokes :: ![Stroke] } 
            deriving Show 
 
 -- | 
-
 getXYtuples :: Stroke -> [(Double,Double)]
 getXYtuples (Stroke _t _c _w d) = map (\(x :!: y) -> (x,y)) d
 getXYtuples (VWStroke _t _c d) = map ((,)<$>fst3<*>snd3) d 
 
+----------------------------
+-- Lenses
+----------------------------
+
 -- | 
-
-s_tool :: Stroke :-> ByteString
-s_tool = lens stroke_tool (\a f -> f { stroke_tool = a })  
+s_tool :: Simple Lens Stroke ByteString
+s_tool = lens stroke_tool (\f a -> f { stroke_tool = a })  
 
 -- | 
+s_color :: Simple Lens Stroke ByteString 
+s_color = lens stroke_color (\f a -> f { stroke_color = a } )
 
-s_color :: Stroke :-> ByteString 
-s_color = lens stroke_color (\a f -> f { stroke_color = a } )
+-- | 
+s_title :: Simple Lens Xournal Title
+s_title = lens xoj_title (\f a -> f { xoj_title = a } )
 
--- s_width :: Stroke :-> Double 
--- s_width = lens stroke_width (\a f -> f { stroke_width = a } )
+-- | 
+s_pages :: Simple Lens Xournal [Page]
+s_pages = lens xoj_pages (\f a -> f { xoj_pages = a } )
 
--- s_data :: Stroke :-> [Pair Double Double] 
--- s_data = lens stroke_data (\a f -> f { stroke_data = a } )
+-- | 
+s_dim :: Simple Lens Page Dimension 
+s_dim = lens page_dim (\f a -> f { page_dim = a } )
 
-s_title :: Xournal :-> Title
-s_title = lens xoj_title (\a f -> f { xoj_title = a } )
+-- | 
+s_bkg :: Simple Lens Page Background 
+s_bkg = lens page_bkg (\f a -> f { page_bkg = a } )
 
-s_pages :: Xournal :-> [Page]
-s_pages = lens xoj_pages (\a f -> f { xoj_pages = a } )
+-- | 
+s_layers :: Simple Lens Page [Layer] 
+s_layers = lens page_layers (\f a -> f { page_layers = a } )
 
-s_dim :: Page :-> Dimension 
-s_dim = lens page_dim (\a f -> f { page_dim = a } )
+-- | 
+s_strokes :: Simple Lens Layer [Stroke]
+s_strokes = lens layer_strokes (\f a -> f { layer_strokes = a } )
 
-s_bkg :: Page :-> Background 
-s_bkg = lens page_bkg (\a f -> f { page_bkg = a } )
+--------------------------
+-- empty objects
+--------------------------
 
-s_layers :: Page :-> [Layer] 
-s_layers = lens page_layers (\a f -> f { page_layers = a } )
-
-s_strokes :: Layer :-> [Stroke]
-s_strokes = lens layer_strokes (\a f -> f { layer_strokes = a } )
-
-
-
+-- | 
 emptyXournal :: Xournal
 emptyXournal = Xournal "" [] 
 
+-- | 
 emptyLayer :: Layer 
 emptyLayer = Layer { layer_strokes = [] }
 
+-- | 
 emptyStroke :: Stroke 
 emptyStroke = Stroke "pen" "black" 1.4 []
 
-
+-- | 
 defaultBackground :: Background
 defaultBackground = Background { bkg_type = "solid"
                                , bkg_color = "white"
                                , bkg_style = "lined" 
                                }
 
+-- | 
 defaultLayer :: Layer
 defaultLayer = Layer { layer_strokes  = [] } 
 
+-- | 
 defaultPage :: Page
 defaultPage = Page { page_dim = Dim  612.0 792.0 
                    , page_bkg = defaultBackground
                    , page_layers = [ defaultLayer ] 
                    } 
 
+-- | 
 defaultXournal :: Xournal 
 defaultXournal = Xournal "untitled" [ defaultPage  ] 
 
-
+-- | 
 newPageFromOld :: Page -> Page
 newPageFromOld page = 
   Page { page_dim = page_dim page 
@@ -179,25 +183,3 @@ newPageFromOld page =
        , page_layers = [emptyLayer] } 
                    
 
-
-{-
-instance IStroke Stroke where
-  strokeTool = stroke_tool
-  strokeColor = stroke_color
-  strokeWidth = stroke_width
-  strokeData = stroke_data
- 
-instance ILayer Layer where
-  type TStroke Layer = Stroke
-  layerStrokes = layer_strokes 
-
-instance IPage Page where
-  type TLayer Page = Layer 
-  pageDim = page_dim 
-  pageBkg = page_bkg 
-  pageLayers = page_layers
-
-instance IXournal Xournal where
-  type TPage Xournal = Page 
-  xournalPages = xoj_pages 
--}

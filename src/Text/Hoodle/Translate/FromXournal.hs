075c5a4d99edfc1df6e50d657499c91835fd0ccc
Ian-Woo Kim <ianwookim@gmail.com>
2012-09-08 17:53:00 -0400

    implement Translate.FromXournal

---
+++ src/Text/Hoodle/Translate/FromXournal.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE RecordWildCards #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Text.Hoodle.Builder 
+-- Copyright   : (c) 2011, 2012 Ian-Woo Kim
+--
+-- License     : BSD3
+-- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
+-- Stability   : experimental
+-- Portability : GHC
+--
+-----------------------------------------------------------------------------
+
+module Text.Hoodle.Translate.FromXournal 
+( mkHoodleFromXournal      
+) where
+
+import qualified Data.Xournal.Simple as X 
+import qualified Data.Hoodle.Simple as H
+
+-- | 
+mkHoodleFromXournal :: X.Xournal -> H.Hoodle 
+mkHoodleFromXournal X.Xournal {..} = 
+    H.Hoodle xoj_title (map x2h4Page xoj_pages) 
+    
+-- |     
+x2h4Page :: X.Page -> H.Page
+x2h4Page X.Page {..} = H.Page 
+                         (x2h4dim page_dim) 
+                         (x2h4bkg page_bkg) 
+                         (map x2h4layer page_layers)
+                         
+-- |                          
+x2h4dim :: X.Dimension -> H.Dimension
+x2h4dim X.Dim {..} = H.Dim dim_width dim_height 
+
+-- | 
+x2h4bkg :: X.Background -> H.Background 
+x2h4bkg X.Background {..} = H.Background bkg_type bkg_color bkg_style
+x2h4bkg X.BackgroundPdf {..} = 
+    H.BackgroundPdf bkg_type bkg_domain bkg_filename bkg_pageno
+
+
+-- | 
+x2h4layer :: X.Layer -> H.Layer 
+x2h4layer X.Layer {..} = H.Layer (map x2h4stroke layer_strokes)
+
+-- | 
+x2h4stroke :: X.Stroke -> H.Stroke 
+x2h4stroke X.Stroke {..} = 
+    H.Stroke stroke_tool stroke_color stroke_width stroke_data
+x2h4stroke X.VWStroke {..} = 
+    H.VWStroke stroke_tool stroke_color stroke_vwdata
+    
+

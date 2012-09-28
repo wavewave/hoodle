5dd3299a5175db1dbfc9327425b5fc9e79618985
Ian-Woo Kim <ianwookim@gmail.com>
2012-09-28 18:14:26 -0400

    update parsetest.hs

--- examples/parsetest.hs
+++ examples/parsetest.hs
@@ -10,30 +10,46 @@ import           System.Environment
 import Data.Hoodle.Simple
 -- 
 import Text.Hoodle.Parse.Attoparsec 
-import Text.Hoodle.Parse.Conduit 
+-- import Text.Hoodle.Parse.Conduit 
+import Graphics.Hoodle.Render
+import Graphics.Hoodle.Render.Type
+import Graphics.Rendering.Cairo 
 
 -- |  
 main :: IO ()   
 main = do 
   args <- getArgs 
-  when (length args /= 2) $ error "parsertest mode filename (mode = atto/sax)"  
+  when (length args /= 3) $ error "parsertest mode filename (mode = atto/sax) outputfile"  
   if args !! 0 == "atto" 
-    then attoparsec (args !! 1)
-    else sax (args !! 1)
+    then attoparsec (args !! 1) (args !! 2)
+    else return () -- sax (args !! 1)
      
      
 -- | using attoparsec without any built-in xml support 
-attoparsec :: FilePath -> IO () 
-attoparsec fp = do 
+attoparsec :: FilePath -> FilePath -> IO () 
+attoparsec fp ofp = do 
   bstr <- B.readFile fp
   let r = parse hoodle bstr
-  
   case r of 
-    Done _ h -> print (length (hoodle_pages h))
-      
+    Done _ h -> renderjob h ofp --  print (length (hoodle_pages h))
     _ -> print r 
 
+-- | 
+renderjob :: Hoodle -> FilePath -> IO () 
+renderjob h ofp = do 
+  let p = head (hoodle_pages h) 
+  let Dim width height = page_dim p  
+  withPDFSurface ofp width height $ \s -> renderWith s $  
+    (sequence1_ showPage . map renderPage . hoodle_pages) h 
+    
+-- | interleaving a monadic action between each pair of subsequent actions
+sequence1_ :: (Monad m) => m () -> [m ()] -> m () 
+sequence1_ _ []  = return () 
+sequence1_ _ [a] = a 
+sequence1_ i (a:as) = a >> i >> sequence1_ i as 
 
+
+{-
 -- | using sax (from xml-conduit)
 sax :: FilePath -> IO () 
 sax fp = do 
@@ -41,3 +57,4 @@ sax fp = do
   case r of 
     Left err -> print err
     Right h -> print (length (hoodle_pages h))
+-}

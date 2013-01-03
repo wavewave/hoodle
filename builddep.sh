ae5162141d4482585f6f3ed7dbfed04d33b97ee3
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 05:10:51 -0500

    use apt-get for travis-ci

--- builddep.sh
+++ builddep.sh
@@ -1,15 +1,13 @@
 #!/bin/bash 
 
-cabal install transformers
-
-ghc-pkg list
+sudo apt-get install libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
 
 mkdir deps
 git clone https://github.com/wavewave/devadmin.git deps/devadmin
 cd deps/devadmin ; cabal install --force-reinstalls ; cd ../../
 $HOME/.cabal/bin/build cloneall --config=build.conf
 
-cabal install gtk2hs-buildtools
+#cabal install gtk2hs-buildtools
 $HOME/.cabal/bin/build bootstrap --config=build.conf
 
 cabal install --enable-tests

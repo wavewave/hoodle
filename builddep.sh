cfde706f4819be1ddd34a87a0c4afa5d95a6222a
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 04:56:58 -0500

    install libghc-hstringtemplate-dev in travis-ci

--- builddep.sh
+++ builddep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
+#cabal install gtk2hs-buildtools
+sudo apt-get install libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
+
+
 mkdir deps
 git clone https://github.com/wavewave/devadmin.git deps/devadmin
 cd deps/devadmin ; cabal install ; cd ../../
 $HOME/.cabal/bin/build cloneall --config=build.conf
 
-#cabal install gtk2hs-buildtools
-sudo apt-get install gtk2hs-buildtools 
-sudo apt-get install libghc-gtk-dev
-sudo apt-get install libghc-gtk-doc 
 $HOME/.cabal/bin/build bootstrap --config=build.conf
 

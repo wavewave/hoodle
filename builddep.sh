662ae37bc9248d6442887e13a2e4c52420742687
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 07:56:31 -0500

    automatic documentation upload

--- builddep.sh
+++ builddep.sh
@@ -1,3 +1,11 @@
-echo $HOODLEAUTHOR
-
 cabal install 
+
+echo "$SRVRPKEY" > whoisit 
+
+tar cvzf doc.tar.gz $HOME/.cabal/doc
+scp doc.tar.gz
+
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i whoisit doc.tar.gz $SRVRID@$SRVR:$HOME/
+
+rm whoisit 
+

6c25c68d3542e478cf4ce21e9ff617ef34e5deee
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 09:43:08 -0500

    missing http://

--- builddep.sh
+++ builddep.sh
@@ -6,7 +6,7 @@ echo "machine $SRVR"$'\n'"login $SRVRID"$'\n'"password $SRVRPKEY" > $HOME/.netrc
 chmod 0600 $HOME/.netrc 
 
 tar cvzf hoodle-types.tar.gz $HOME/.cabal/share/doc/hoodle-types*
-echo "open $SRVR:$SRVRPORT$SRVRDIR"$'\n'"put hoodle-types.tar.gz"$'\n'" "  > script 
+echo "open http://$SRVR:$SRVRPORT$SRVRDIR"$'\n'"put hoodle-types.tar.gz"$'\n'" "  > script 
 
 
 #chmod 0600 whoisit

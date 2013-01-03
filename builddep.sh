937c53ec5122f0c19a4040532be6fb23d99704f4
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 08:16:12 -0500

    share/doc

--- builddep.sh
+++ builddep.sh
@@ -2,7 +2,7 @@ cabal install --enable-documentation
 
 echo "$SRVRPKEY" > whoisit 
 
-tar cvzf doc.tar.gz $HOME/.cabal/doc
+tar cvzf doc.tar.gz $HOME/.cabal/share/doc
 scp doc.tar.gz
 
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i whoisit doc.tar.gz $SRVRID@$SRVR:$HOME/

1f33c3561ab155271179c8d5a9da19695cd66280
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 09:24:34 -0500

    use cadaver for upload haddock

--- builddep.sh
+++ builddep.sh
@@ -1,11 +1,21 @@
 cabal install --enable-documentation 
 
-echo "$SRVRPKEY" > whoisit 
+sudo apt-get install cadaver
 
-tar cvzf doc.tar.gz $HOME/.cabal/share/doc
-scp doc.tar.gz
+echo "login $SRVRID"$'\n'"password $SRVRPKEY" > $HOME/.netrc 
+chmod 0600 $HOME/.netrc 
 
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i whoisit doc.tar.gz $SRVRID@$SRVR:$HOME/
+tar cvzf hoodle-types.tar.gz $HOME/.cabal/share/doc/hoodle-types*
+echo "open $SRVR"$'\n'"put hoodle-types.tar.gz"$'\n'" "  > script 
 
-rm whoisit 
+
+#chmod 0600 whoisit
+
+
+#scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i whoisit hoodle-types.tar.gz $SRVRID@$SRVR:$HOME/
+
+cadaver < script  
+
+rm script 
+rm $HOME/.netrc 
 

26e56ecfb8a359c22d5666dc38e127a0de40bb01
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 04:51:38 -0500

    using apt-get for travis-ci

--- builddep.sh
+++ builddep.sh
@@ -5,6 +5,9 @@ git clone https://github.com/wavewave/devadmin.git deps/devadmin
 cd deps/devadmin ; cabal install ; cd ../../
 $HOME/.cabal/bin/build cloneall --config=build.conf
 
-cabal install gtk2hs-buildtools
+#cabal install gtk2hs-buildtools
+sudo apt-get install gtk2hs-buildtools 
+sudo apt-get install libghc-gtk-dev
+sudo apt-get install libghc-gtk-doc 
 $HOME/.cabal/bin/build bootstrap --config=build.conf
 

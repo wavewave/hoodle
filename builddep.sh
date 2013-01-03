bb4738c3b8d75f5c82386f4471a22363b945a574
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 10:51:53 -0500

    autogenerate documentation using travis-ci

--- builddep.sh
+++ builddep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sudo apt-get install libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
+sudo apt-get install cadaver libghc-hscolour-dev libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
 
 mkdir deps
 git clone https://github.com/wavewave/devadmin.git deps/devadmin
@@ -9,4 +9,17 @@ $HOME/.cabal/bin/build cloneall --config=build.conf
 
 #cabal install gtk2hs-buildtools
 $HOME/.cabal/bin/build bootstrap --config=build.conf
+
+build haddockboot 
+
+echo "machine $SRVR"'\n'"login $SRVRID"'\n'"password $SRVRPKEY" > $HOME/.netrc 
+chmod 0600 $HOME/.netrc 
+
+tar cvzf hoodle-core.tar.gz $HOME/.cabal/share/doc/hoodle* $HOME/.cabal/share/doc/xournal* $HOME/.cabal/share/doc/coroutine-object*
+echo "open http://$SRVR:$SRVRPORT$SRVRDIR"'\n'"put hoodle-core.tar.gz"'\n'" "  > script 
+
+cadaver < script  
+
+rm script 
+rm $HOME/.netrc 
 

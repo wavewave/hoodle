5cc6e0eff6fff79b88b62d27892f367d65c0e288
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-04 21:33:23 -0500

    support JPG file using gd library

--- src/Graphics/Hoodle/Render/Item.hs
+++ src/Graphics/Hoodle/Render/Item.hs
@@ -16,9 +16,15 @@
 
 module Graphics.Hoodle.Render.Item where
 
+import           Control.Applicative
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C8
+import           Graphics.GD.ByteString
 import           Graphics.Rendering.Cairo
 import qualified Graphics.Rendering.Cairo.SVG as RSVG
+import           System.Directory
+import           System.Environment 
+import           System.FilePath 
 -- from hoodle-platform 
 import Data.Hoodle.BBox 
 import Data.Hoodle.Simple
@@ -31,11 +37,31 @@ cnstrctRItem (ItemStroke strk) = return (RItemStroke (mkStrokeBBox strk))
 cnstrctRItem (ItemImage img) = do 
     let imgbbx = mkImageBBox img
         filesrc = C8.unpack (img_src img)
-    sfc <- imageSurfaceCreateFromPNG filesrc
+        filesrcext = takeExtension filesrc 
+        imgaction 
+          | filesrcext == "PNG" || filesrcext == "png" = 
+              Just <$> imageSurfaceCreateFromPNG filesrc
+          | filesrcext == "JPG" || filesrcext == "jpg" = 
+              Just <$> getJPGandCreateSurface filesrc 
+          | otherwise = return Nothing 
+    msfc <- imgaction
     -- rendering is not implemented yet
-    return (RItemImage imgbbx (Just sfc))
+    return (RItemImage imgbbx msfc)
 cnstrctRItem (ItemSVG svg@(SVG _ _ bstr (x,y) _)) = do 
     let str = C8.unpack bstr 
         svgbbx = mkSVGBBox svg
     rsvg <- RSVG.svgNewFromString str
     return (RItemSVG svgbbx (Just rsvg))
+
+
+-- | read JPG file using GD library and create cairo image surface
+--   currently, this uses temporary png file (which is potentially dangerous)
+getJPGandCreateSurface :: FilePath -> IO Surface 
+getJPGandCreateSurface fp = do 
+    img <- loadJpegFile fp  
+    bstr <- savePngByteString img
+    tdir <- getTemporaryDirectory 
+    let tfile = tdir </> "temp.png"
+    B.writeFile tfile bstr 
+    imageSurfaceCreateFromPNG tfile 
+

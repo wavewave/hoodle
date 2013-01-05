3ae6a14006a38bad9cb172b9a7b2722a8a3fb45c
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-04 21:56:46 -0500

    add gd dep in builddep.sh

--- builddep.sh
+++ builddep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 #cabal install gtk2hs-buildtools
-sudo apt-get install libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
+sudo apt-get install libgd2-xpm-dev libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
 
 
 mkdir deps

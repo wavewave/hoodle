8f6b508040f42250a107944146c3de76067a47ea
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-28 12:40:35 -0500

    show version correctly and show other program info

--- lib/Hoodle/ProgType.hs
+++ lib/Hoodle/ProgType.hs
@@ -3,21 +3,41 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Hoodle.ProgType 
--- Copyright   : (c) 2011, 2012 Ian-Woo Kim
+-- Copyright   : (c) 2011-2013 Ian-Woo Kim
 --
 -- License     : BSD3
 -- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
 -- Stability   : experimental
 -- Portability : GHC
 --
+-----------------------------------------------------------------------------
 
 module Hoodle.ProgType where 
 
+import Data.Version 
 import System.Console.CmdArgs
+-- 
+import Paths_hoodle 
+
 
 data Hoodle = Test { xojfile :: Maybe FilePath
                      }  
               deriving (Show,Data,Typeable)
+
+programName :: String 
+programName = "Hoodle" 
+
+programVersion :: String 
+programVersion = showVersion version 
+
+programInfo :: String 
+programInfo = programName ++ " version " ++ programVersion 
+
+programAbout :: String 
+programAbout = "A pen notetaking program written in haskell" 
+
+programCopyright :: String 
+programCopyright = "(C) Ian-Woo Kim 2011-2013"
 
 test :: Hoodle
 test = Test { xojfile = def &= typ "FILENAME" &= args -- &= argPos 0 &= opt "OPTIONAL" 
@@ -26,5 +46,12 @@ test = Test { xojfile = def &= typ "FILENAME" &= args -- &= argPos 0 &= opt "OPT
 
 mode :: Hoodle
 mode = modes [test] 
+       &= versionArg [ explicit, name "version", name "V", summary programInfo] 
+       &= summary (programInfo ++ ", " ++ programCopyright) 
+       &= help programAbout
+       &= helpArg [explicit, name "help", name "h"]
+       &= program programName 
+
+
 
 

a4a330d10a74def5f1db8623d046df309f5b5301
Ian-Woo Kim <ianwookim@gmail.com>
2013-02-09 12:32:47 -0500

    add Primitive.hs

---
+++ src/Data/Hoodle/Primitive.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Data.Hoodle.Primitive
+-- Copyright   : (c) 2013 Ian-Woo Kim
+--
+-- License     : BSD3
+-- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- primitive types for hoodle
+-- 
+-----------------------------------------------------------------------------
+
+module Data.Hoodle.Primitive where

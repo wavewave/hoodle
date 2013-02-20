751ced838f7526d7c8ba00ad0a3e39ee3efca6ec
Ian-Woo Kim <ianwookim@gmail.com>
2013-02-19 22:46:56 -0500

    clear pdf export (not blurred image)

--- src/Hoodle/Coroutine/File.hs
+++ src/Hoodle/Coroutine/File.hs
@@ -225,11 +225,12 @@ renderjob :: RHoodle -> FilePath -> IO ()
 renderjob h ofp = do 
   let p = maybe (error "renderjob") id $ IM.lookup 0 (view gpages h)  
   let Dim width height = view gdimension p  
-  let rf :: InBBox RPage -> Render ()
-      rf x = cairoRenderOption (InBBoxOption Nothing) x >> return ()  
+  let -- rf :: InBBox RPage -> Render ()
+      -- rf x = cairoRenderOption (InBBoxOption Nothing) x >> return ()  
+      rf x = cairoRenderOption (RBkgDrawPDF,DrawFull) x >> return () 
   withPDFSurface ofp width height $ \s -> renderWith s $  
     -- (sequence1_ showPage . map renderPage . hoodle_pages) h 
-    (sequence1_ showPage . map (rf . InBBox) . IM.elems . view gpages ) h 
+    (sequence1_ showPage . map rf . IM.elems . view gpages ) h 
 
 -- | 
 fileExport :: MainCoroutine ()

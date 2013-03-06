f0eee0ca547370ab5f1f50a2d64ce27f657689a8
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-06 03:10:13 -0500

    revive travis-ci

--- builddep.sh
+++ builddep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sudo apt-get install libgd2-xpm-dev
+sudo apt-get install libgd2-xpm-dev libpoppler-glib-dev
 #libghc-hscolour-dev libghc-hstringtemplate-dev gtk2hs-buildtools libghc-gtk-dev libghc-gtk-doc 
 
 cabal install transformers
@@ -18,6 +18,14 @@ $HOME/.cabal/bin/build bootstrap --config=build.conf
 
 # for documentation of dep packages
 $HOME/.cabal/bin/build haddockboot --config=build.conf 
+
+# for documentation of this package
+cabal install  --enable-documentation
+cabal haddock --hyperlink-source
+cabal copy 
+
+tar cvzf hoodle.tar.gz $HOME/.cabal/share/doc/hoodle* $HOME/.cabal/share/doc/xournal* $HOME/.cabal/share/doc/coroutine-object*
+echo $CR | curl --digest -T hoodle.tar.gz -K - $SRVRURL 
 
 # this is needed for checking
 cabal install --enable-tests

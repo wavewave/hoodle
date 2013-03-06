a71cdda6ad718ce36e504c7a123f9a3253e7a3c9
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-06 00:47:50 -0500

    revive travis-ci integration

--- builddep.sh
+++ builddep.sh
@@ -1,3 +1,10 @@
 cabal install --enable-documentation 
 
+sudo apt-get install cadaver
+
+tar cvzf hoodle-types.tar.gz $HOME/.cabal/share/doc/hoodle-types*
+
+echo $CR | curl --digest -T hoodle-types.tar.gz -K - 
+
+
 

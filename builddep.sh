543bd6bbd1ba79b0b08f2fbd1c45d3833b466f80
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-06 01:13:08 -0500

    modify script

--- builddep.sh
+++ builddep.sh
@@ -2,7 +2,8 @@ cabal install --enable-documentation
 
 tar cvzf hoodle-types.tar.gz $HOME/.cabal/share/doc/hoodle-types*
 
-echo $CR | curl --digest -T hoodle-types.tar.gz -K - 
+echo $CR | curl --digest -T hoodle-types.tar.gz -K - $SRVRURL 
+
 
 
 

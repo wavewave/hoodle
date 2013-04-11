fe6a42e350d443c9f948628d82925e5d208492c8
Ian-Woo Kim <ianwookim@gmail.com>
2013-04-11 01:01:53 -0400

    version bump to hoodle-0.2.0.999

--- exe/hoodle.hs
+++ exe/hoodle.hs
@@ -1,9 +1,9 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Main
--- Copyright   : (c) 2011, 2012 Ian-Woo Kim
+-- Copyright   : (c) 2011-2013 Ian-Woo Kim
 --
--- License     : BSD3
+-- License     : GPL-3
 -- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
 -- Stability   : experimental
 -- Portability : GHC

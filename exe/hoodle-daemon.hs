e7556c37083e1fb0d862d339c8f0f749f12fc0ac
Ian-Woo Kim <ianwookim@gmail.com>
2013-12-14 12:28:17 -0500

    working dbus

--- exe/hoodle-daemon.hs
+++ exe/hoodle-daemon.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.List (sort)
+import DBus
+import DBus.Client
+
 main :: IO ()
 main = do
   putStrLn "hoodle daemon version 0.0.999"
   
+  client <- connectSession 
+  
+  reply <- call_ client (methodCall "/org/freedesktop/DBus" "org.freedesktop.DBus" "ListNames")
+    { methodCallDestination = Just "org.freedesktop.DBus"
+    }
+           
+  let Just names = fromVariant (methodReturnBody reply !! 0)
+  mapM_ putStrLn (sort names)
+  
+

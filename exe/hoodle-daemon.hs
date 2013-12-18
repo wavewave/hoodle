12ffeaca1810b445761077bc4a6b98b5a09627e8
Ian-Woo Kim <ianwookim@gmail.com>
2013-12-18 04:44:44 -0500

    socket restart using NetworkManager d-bus signal

--- exe/hoodle-daemon.hs
+++ exe/hoodle-daemon.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever)
+import Control.Monad (forever, when)
+import Data.Foldable (forM_)
+import Data.Word (Word32(..))
 import Data.IORef
 import Data.List (sort)
+import Data.Maybe (mapMaybe)
+import qualified Data.Text as T
 import DBus
 import DBus.Client
 import System.Directory
@@ -28,13 +33,24 @@ checkOtherInst client act = do
     
 onResume :: IORef ProcessHandle -> Signal -> IO ()
 onResume ref signal = do -- print signal
-                         threadDelay 5000000
-                         ph <- readIORef ref
-                         terminateProcess ph
-                         putStrLn "I got back from sleep mode"
-                         ph' <- runsocket
-                         writeIORef ref ph'
-  
+                         let xs :: [Dictionary] = (mapMaybe fromVariant . signalBody) signal
+                         when ((not.null) xs) $ do 
+                           let x = dictionaryItems (head xs)                               
+                           case (lookup (toVariant ("State" :: T.Text) ) x) of 
+                            Just v -> do let n = do v' <- fromVariant v :: Maybe Variant 
+                                                    v'' <- fromVariant v' :: Maybe Word32
+                                                    return v''
+                                         print n
+                                         forM_ n $ \n' -> 
+                                           when (n' == 100) $ do 
+                                             threadDelay 1000000
+                                             ph <- readIORef ref
+                                             terminateProcess ph
+                                             putStrLn "network restart"
+                                             ph' <- runsocket
+                                             writeIORef ref ph'
+                            _ -> return ()
+                         
 runsocket = do 
   hdir <- getHomeDirectory
   (_,_,_,ph) <- createProcess
@@ -58,8 +74,9 @@ main = do
     ph <- runsocket
     ref <- newIORef ph
     forkIO $ do 
-      listen clientSys matchAny { matchPath = Just "/org/freedesktop/UPower" 
-                                , matchMember = Just "NotifyResume" 
+      listen clientSys matchAny { matchPath = Just "/org/freedesktop/NetworkManager/Devices/0" 
+                                , matchInterface = Just "org.freedesktop.NetworkManager.Device.Wireless"
+                                , matchMember = Just "PropertiesChanged" 
                                 }
              (onResume ref) 
     forever $ getLine

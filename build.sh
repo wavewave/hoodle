1dce759a5f9c187a65ed5351178ad62707c10a8b
Greg Weber <greg@gregweber.info>
2014-01-04 11:14:10 -0800

    support cabal-meta install

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-pushd coroutine-object; cabal install ; popd 
-pushd xournal-types ; cabal install ; popd 
-pushd xournal-parser; cabal install ; popd
-pushd hoodle-types ; cabal install ; popd 
-pushd hoodle-parser ; cabal install ; popd 
-pushd hoodle-builder ; cabal install ; popd
-pushd hoodle-render ; cabal install ; popd 
-pushd hoodle-core ; cabal install ; popd 
-pushd hoodle ; cabal install ; popd 
-
+# our use cabal-meta
+#
+#    cabal-meta install
+cat sources.txt | xargs cabal install -j

dae9256416900693522f365c906ad783f52b09de
Ian-Woo Kim <ianwookim@gmail.com>
2014-06-29 15:02:02 -0400

    create instances of PersistField and PersistFieldSql for MD5 (newtype wrapper for bytestring)

--- hoodle-manage/src/Hoodle/Manage.hs
+++ hoodle-manage/src/Hoodle/Manage.hs
@@ -8,12 +8,14 @@
 
 module Hoodle.Manage where
 
+
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Logger (MonadLogger(..))
 import Control.Monad.Trans.Control (MonadBaseControl(..))
 import Control.Monad.Trans.Resource (MonadResource(..))
 import Data.Conduit (($$))
 import Data.Conduit.List as CL
+-- import Data.Digest.Pure.MD5
 import Data.Text (Text)
 -- import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 -- import Data.UUID
@@ -23,12 +25,17 @@ import Database.Persist.Sqlite (runSqlite, runMigrationSilent)
 import Database.Persist.TH (mkPersist, mkMigrate, persistLowerCase, share, sqlSettings)
 import Database.Persist.Sql (rawQuery)
 -- 
+import Hoodle.Manage.Type.MD5
 import Hoodle.Manage.Type.UUID
+
+
+import Crypto.Hash.MD5
+import Data.ByteString.Base16 as B16
 
 share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
   HoodleFile 
     uuid     UUID
-    md5hash  Text    
+    md5hash  MD5 
     location Text
     deriving Show
 |]
@@ -36,7 +43,8 @@ share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
 -- sqlite
 
 maintest :: IO () 
-maintest = 
+maintest = do 
+
   runSqlite ":memory:" $ do 
          buildDB
          liftIO (putStrLn "dump hoodle")
@@ -54,7 +62,10 @@ buildDB :: (Monad m, MonadLogger m, MonadIO m, MonadResource m, MonadBaseControl
 buildDB = do
     runMigration migrateTables
     uuid <- liftIO $ nextRandom
-    insert $ HoodleFile uuid "testtttt" "file://url.com/file.hdl"
+    liftIO $   print (B16.encode (hash "dlkjfldfkfj"))
+    let testmd5 = MD5 (hash "dlkjfldfkfj")
+
+    insert $ HoodleFile uuid testmd5 "file://url.com/file.hdl"
 
 dumphdl :: (MonadResource m, MonadLogger m) => SqlPersistT m ()
 dumphdl = rawQuery "select * from Hoodle_File" [] $$ CL.mapM_ (liftIO . print)

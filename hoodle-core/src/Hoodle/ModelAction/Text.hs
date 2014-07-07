83fa458f4c609934bed690a059f648a2c6a4449c
Ian-Woo Kim <ianwookim@gmail.com>
2014-07-07 17:50:26 +0000

    implemented KeywordMap and KeywordContent parsing

--- hoodle-core/src/Hoodle/ModelAction/Text.hs
+++ hoodle-core/src/Hoodle/ModelAction/Text.hs
@@ -17,6 +17,7 @@ module Hoodle.ModelAction.Text where
 import           Control.Applicative
 import           Data.Attoparsec.Text as A
 import           Data.Char (isAlphaNum)
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TIO
@@ -32,22 +33,27 @@ getLinesFromText (i,e) = T.unlines . Prelude.drop (i-1) . Prelude.take e . T.lin
 -- |
 getKeywordContent :: T.Text -- ^ keyword 
                   -> T.Text -- ^ tex file 
-                  -> Maybe (T.Text, T.Text) -- [ (T.Text, T.Text) ] -- Maybe T.Text -- ^ subpart 
-getKeywordContent k txt = 
-    case parseOnly keywordContents txt of 
-      Left err -> trace (show err) $ Nothing
-      Right r -> Just r
+                  -> Maybe T.Text -- Maybe T.Text -- ^ subpart 
+getKeywordContent k txt = M.lookup k (getKeywordMap txt)
 
 
+-- | 
+getKeywordMap :: T.Text -> M.Map T.Text T.Text
+getKeywordMap txt = case parseOnly (many keywordContents) txt of 
+                      Left err -> trace (show err) $ M.empty
+                      Right lst -> M.fromList lst
 
 
+-- |
 keywordBegin :: A.Parser T.Text
 keywordBegin = 
-    string "%h%k " *> A.skipSpace 
-                   *> A.takeWhile1 isAlphaNum 
-                   <* A.skipWhile (not . isEndOfLine)
-                   <* A.endOfLine
+    skipWhile (notInClass "%") *> (try (string "%h%k " *> A.skipSpace 
+                                        *> A.takeWhile1 isAlphaNum 
+                                        <* A.skipWhile (not . isEndOfLine)
+                                        <* A.endOfLine)
+                                   <|> (oneline *> keywordBegin))
 
+-- | 
 keywordEnd :: A.Parser ()
 keywordEnd =  string "%h%k%end" >> skipWhile (notInClass "\r\n") >> endOfLine
 

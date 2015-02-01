a99caf39229a1c0b1a297515e32280d6e3e3f93b
Ian-Woo Kim <ianwookim@gmail.com>
2015-02-01 17:51:29 -0500

    successful typed communication with hub server

--- hoodle-core/src/Hoodle/Coroutine/Socket.hs
+++ hoodle-core/src/Hoodle/Coroutine/Socket.hs
@@ -5,7 +5,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Hoodle.Coroutine.Socket
--- Copyright   : (c) 2014 Ian-Woo Kim
+-- Copyright   : (c) 2014, 2015 Ian-Woo Kim
 --
 -- License     : BSD3
 -- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
@@ -22,13 +22,19 @@ import qualified Control.Exception as E
 import           Control.Lens (view)
 import           Control.Monad.IO.Class
 import           Control.Monad.State
+import           Control.Monad.Trans.Either
 import           Control.Monad.Trans.Maybe
+import qualified Data.Aeson.Encode as AE
+import           Data.Aeson.Parser
+import           Data.Aeson.Types
+import qualified Data.Attoparsec.ByteString as A
 import qualified Data.CaseInsensitive as CI
 import qualified Data.Foldable as F
-import Data.Monoid ((<>))
-import Data.Text (pack,unpack)
-import Data.Text.Encoding (encodeUtf8)
-import Data.Time.Clock
+import qualified Data.HashMap.Strict as HM
+import           Data.Monoid ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import           Data.Time.Clock
 import qualified Graphics.UI.Gtk as Gtk
 import Network
 import Network.Google.OAuth2 (formUrl, exchangeCode, refreshTokens,
@@ -43,6 +49,7 @@ import System.Info (os)
 import System.Process (rawSystem)
 --
 import Hoodle.Coroutine.Dialog
+import Hoodle.Coroutine.HubInternal
 import Hoodle.Script.Hook
 import Hoodle.Type.Coroutine
 import Hoodle.Type.Event
@@ -52,6 +59,25 @@ import Hoodle.Util
 --
 
 -- data Message = Message { msgbody :: Text } deriving (Show,Eq,Ord)
+data HoodleWSEvent = HWSOpen { hws_filepath :: T.Text }
+                   deriving Show
+
+instance ToJSON HoodleWSEvent where
+  toJSON HWSOpen {..} = object [ "eventType" .= toJSON ("open" :: T.Text) 
+                               , "filepath"  .= toJSON hws_filepath ]
+
+instance FromJSON HoodleWSEvent where
+  parseJSON (Object v) =
+    let r = do 
+          String typ <- HM.lookup "eventType" v
+          case typ of
+            "open" -> do String fp <- HM.lookup "filepath" v
+                         return HWSOpen { hws_filepath = fp }
+            _ -> Nothing
+    in case r of
+         Nothing -> fail "error in parsing HoodleWSEvent"
+         Just result -> return result
+  parseJSON _ = fail "error in parsing HoodleWSEvent"
 
 -- |
 socketConnect :: MainCoroutine ()
@@ -67,10 +93,10 @@ socketConnect = do
 
 
 socketWork :: HubInfo -> MainCoroutine ()
-socketWork HubInfo {..} = do
+socketWork hinfo@HubInfo {..} = do
     hdir <- liftIO $ getHomeDirectory
     let tokfile = hdir </> ".hoodle.d" </> "token.txt"
-        client = OAuth2Client { clientId = unpack cid, clientSecret = unpack secret }
+        client = OAuth2Client { clientId = T.unpack cid, clientSecret = T.unpack secret }
         permissionUrl = formUrl client ["email"]
     liftIO (doesFileExist tokfile) >>= \b -> unless b $ do       
       msgShout $ "Load this URL: "++show permissionUrl
@@ -81,10 +107,27 @@ socketWork HubInfo {..} = do
       mauthcode <- textInputDialog "Please paste the verification code: "
       F.forM_ mauthcode $ \authcode -> do
         tokens   <- liftIO $ exchangeCode client authcode
-        -- msgShout $ "Received access token: "++show (accessToken tokens)
         liftIO $ writeFile tokfile (show tokens)
     doIOaction $ \evhandler -> do 
       forkIO $ (`E.catch` (\(err:: E.SomeException)-> print err >> return ())) $ 
+        withHub hinfo tokfile $ \manager coojar -> do
+          request2' <- parseUrl ("http://" <> hubsocketurl <> ":" <> show hubsocketport </> hubsocketpath)
+          ctime <- liftIO getCurrentTime
+          let (bstr,_) = computeCookieString request2' coojar ctime True
+              newheaders = [(CI.mk "Cookie",bstr)] 
+          liftIO $ WS.runClientWith hubsocketurl hubsocketport hubsocketpath WS.defaultConnectionOptions newheaders $ \conn -> forever $ do 
+            txt <- WS.receiveData conn
+            runEitherT $ do
+              v <- hoistEither $ A.parseOnly json (TE.encodeUtf8 txt)
+              o :: HoodleWSEvent <- hoistEither $ parseEither parseJSON v
+              let urlpath = FileUrl (hubfileroot </> T.unpack (hws_filepath o)) 
+              (liftIO . Gtk.postGUIAsync . evhandler . UsrEv) (OpenLink urlpath Nothing)
+      return (UsrEv ActionOrdered)
+
+
+
+
+{-
         withSocketsDo $ withManager $ \manager -> do
           -- refresh token
           oldtok <- liftIO $ read <$> (readFile tokfile)
@@ -102,18 +145,4 @@ socketWork HubInfo {..} = do
           response <- httpLbs request manager
           let coojar = responseCookieJar response
           -- liftIO $ print coojar
-          request2' <- parseUrl ("http://" <> hubsocketurl <> ":" <> show hubsocketport </> hubsocketpath)
-          ctime <- liftIO getCurrentTime
-          let (bstr,_) = computeCookieString request2' coojar ctime True
-              newheaders = [(CI.mk "Cookie",bstr)] 
-          liftIO $ WS.runClientWith hubsocketurl hubsocketport hubsocketpath WS.defaultConnectionOptions newheaders $ \conn -> forever $ do 
-            putStrLn "connected"
-            txt <- WS.receiveData conn
-            
-            let urlpath = FileUrl (hubfileroot </> unpack txt) 
-            (Gtk.postGUIAsync . evhandler . UsrEv) (OpenLink urlpath Nothing)
-
-      return (UsrEv ActionOrdered)
-
-
-
+-}

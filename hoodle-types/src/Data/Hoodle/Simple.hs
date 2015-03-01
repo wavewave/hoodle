968c00a44af611f9fcf26807e17c2a35f0f6f79b
Ian-Woo Kim <ianwookim@gmail.com>
2015-03-01 19:31:23 +0000

    add (From/To)JSON instances for Data.Hoodle.Simple

--- hoodle-types/src/Data/Hoodle/Simple.hs
+++ hoodle-types/src/Data/Hoodle/Simple.hs
@@ -1,5 +1,10 @@
-{-# LANGUAGE BangPatterns, OverloadedStrings,  TypeOperators, 
-             TypeFamilies, FlexibleContexts, RecordWildCards #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -----------------------------------------------------------------------------
 -- |
@@ -18,11 +23,15 @@ module Data.Hoodle.Simple where
 -- from other packages
 import           Control.Applicative 
 import           Control.Lens 
-import           Data.ByteString.Char8 hiding (map)
+import           Data.Aeson.TH
+import           Data.Aeson.Types hiding (Pair(..))
+import           Data.Char
+import           Data.ByteString.Char8 hiding (map,drop)
 import           Data.UUID.V4 
 import qualified Data.Serialize as SE
 import           Data.Strict.Tuple
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 -- from this package
 import           Data.Hoodle.Util
 -- 
@@ -31,13 +40,35 @@ import Prelude hiding (putStrLn,fst,snd,curry,uncurry)
 -- | 
 type Title = ByteString
 
--- | wrapper of object embeddable in Layer
-data Item = ItemStroke Stroke
-          | ItemImage Image
-          | ItemSVG SVG
-          | ItemLink Link
-          | ItemAnchor Anchor
-          deriving (Show,Eq,Ord)
+--  $(deriveJSON defaultOptions ''ByteString)
+
+instance ToJSON ByteString where
+    toJSON = String . TE.decodeUtf8
+
+instance FromJSON ByteString where
+    parseJSON v = TE.encodeUtf8 <$> parseJSON v 
+
+-- |    
+instance (SE.Serialize a, SE.Serialize b) => SE.Serialize (Pair a b) where
+    put (x :!: y) = SE.put x
+                    >> SE.put y
+    get = (:!:) <$> SE.get <*> SE.get
+
+$(deriveJSON defaultOptions {constructorTagModifier = const "Pair"} ''Pair)
+
+
+-- | 
+data Dimension = Dim { dim_width :: !Double, dim_height :: !Double }
+               deriving (Show,Eq,Ord)
+
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 4} ''Dimension)
+
+
+-- | 
+instance SE.Serialize Dimension where 
+  put (Dim w h) = SE.put w >> SE.put h 
+  get = Dim <$> SE.get <*> SE.get
+
 
 
 -- | Pen stroke item 
@@ -52,12 +83,39 @@ data Stroke = Stroke { stroke_tool  :: !ByteString
                        }
             deriving (Show,Eq,Ord)
 
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 7 } ''Stroke)
+
+instance SE.Serialize Stroke where
+    put Stroke{..} = SE.putWord8 0 
+                     >> SE.put stroke_tool 
+                     >> SE.put stroke_color
+                     >> SE.put stroke_width
+                     >> SE.put stroke_data
+    put VWStroke{..} = SE.putWord8 1 
+                       >> SE.put stroke_tool 
+                       >> SE.put stroke_color
+                       >> SE.put stroke_vwdata
+    get = do tag <- SE.getWord8  
+             case tag of 
+               0 -> Stroke <$> SE.get <*> SE.get <*> SE.get <*> SE.get
+               1 -> VWStroke <$> SE.get <*> SE.get <*> SE.get
+               _ -> fail "err in Stroke parsing"
+
 -- | Image item 
 data Image = Image { img_src :: ByteString
                    , img_pos :: (Double,Double)
                    , img_dim :: !Dimension
                    } 
              deriving (Show,Eq,Ord)
+
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 4}  ''Image)
+
+instance SE.Serialize Image where
+    put Image {..} = SE.put img_src
+                     >> SE.put img_pos
+                     >> SE.put img_dim
+    get = Image <$> SE.get <*> SE.get <*> SE.get
+
 
 data SVG = SVG { svg_text :: Maybe ByteString
                , svg_command :: Maybe ByteString 
@@ -66,7 +124,18 @@ data SVG = SVG { svg_text :: Maybe ByteString
                , svg_dim :: !Dimension }  
            deriving (Show,Eq,Ord)
                     
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 4} ''SVG)
 
+-- | 
+instance SE.Serialize SVG where
+    put SVG {..} = SE.put svg_text
+                   >> SE.put svg_command 
+                   >> SE.put svg_render
+                   >> SE.put svg_pos
+                   >> SE.put svg_dim
+    get = SVG <$> SE.get <*> SE.get <*> SE.get <*> SE.get <*> SE.get
+    
+-- |
 data Link = Link { link_id :: ByteString 
                  , link_type :: ByteString 
                  , link_location :: ByteString
@@ -94,50 +163,8 @@ data Link = Link { link_id :: ByteString
 
            deriving (Show,Eq,Ord)                    
 
-data Anchor = Anchor { anchor_id :: ByteString 
-                     , anchor_render :: ByteString
-                     , anchor_pos :: (Double, Double)
-                     , anchor_dim :: !Dimension
-                     } 
-            deriving (Show,Eq,Ord)
-                    
--- | 
-instance SE.Serialize Stroke where
-    put Stroke{..} = SE.putWord8 0 
-                     >> SE.put stroke_tool 
-                     >> SE.put stroke_color
-                     >> SE.put stroke_width
-                     >> SE.put stroke_data
-    put VWStroke{..} = SE.putWord8 1 
-                       >> SE.put stroke_tool 
-                       >> SE.put stroke_color
-                       >> SE.put stroke_vwdata
-    get = do tag <- SE.getWord8  
-             case tag of 
-               0 -> Stroke <$> SE.get <*> SE.get <*> SE.get <*> SE.get
-               1 -> VWStroke <$> SE.get <*> SE.get <*> SE.get
-               _ -> fail "err in Stroke parsing"
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 5 } ''Link) 
 
-
--- | 
-instance SE.Serialize Image where
-    put Image {..} = SE.put img_src
-                     >> SE.put img_pos
-                     >> SE.put img_dim
-    get = Image <$> SE.get <*> SE.get <*> SE.get
-
--- | 
-instance SE.Serialize SVG where
-    put SVG {..} = SE.put svg_text
-                   >> SE.put svg_command 
-                   >> SE.put svg_render
-                   >> SE.put svg_pos
-                   >> SE.put svg_dim
-    get = SVG <$> SE.get <*> SE.get <*> SE.get <*> SE.get <*> SE.get
-    
-    
-
--- | 
 instance SE.Serialize Link where
     put Link {..}       = SE.putWord8 0 
                           >> SE.put link_id
@@ -176,6 +203,14 @@ instance SE.Serialize Link where
                _ -> fail "err in Link parsing"
 
 
+data Anchor = Anchor { anchor_id :: ByteString 
+                     , anchor_render :: ByteString
+                     , anchor_pos :: (Double, Double)
+                     , anchor_dim :: !Dimension
+                     } 
+            deriving (Show,Eq,Ord)
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 7 } ''Anchor )
 
 instance SE.Serialize Anchor where
     put Anchor {..} = SE.put anchor_id
@@ -184,6 +219,15 @@ instance SE.Serialize Anchor where
                       >> SE.put anchor_dim
     get = Anchor <$> SE.get <*> SE.get <*> SE.get <*> SE.get
 
+-- | wrapper of object embeddable in Layer
+data Item = ItemStroke Stroke
+          | ItemImage Image
+          | ItemSVG SVG
+          | ItemLink Link
+          | ItemAnchor Anchor
+          deriving (Show,Eq,Ord)
+
+$(deriveJSON defaultOptions ''Item)
 
 -- | 
 instance SE.Serialize Item where
@@ -205,22 +249,7 @@ instance SE.Serialize Item where
                3 -> ItemLink <$> SE.get
                4 -> ItemAnchor <$> SE.get
                _ -> fail "err in Item parsing"
-
--- |    
-instance (SE.Serialize a, SE.Serialize b) => SE.Serialize (Pair a b) where
-    put (x :!: y) = SE.put x
-                    >> SE.put y
-    get = (:!:) <$> SE.get <*> SE.get
-
-    
--- | 
-data Dimension = Dim { dim_width :: !Double, dim_height :: !Double }
-               deriving (Show,Eq,Ord)
-
--- | 
-instance SE.Serialize Dimension where 
-  put (Dim w h) = SE.put w >> SE.put h 
-  get = Dim <$> SE.get <*> SE.get
+                    
 
 -- | 
 data Background = Background { bkg_type :: !ByteString 
@@ -235,6 +264,9 @@ data Background = Background { bkg_type :: !ByteString
                 | BackgroundEmbedPdf { bkg_type :: ByteString
                                      , bkg_pageno :: Int } 
                 deriving Show 
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 4 } ''Background )
+
 -- | 
 data Revision = Revision { _revmd5 :: !ByteString 
                          , _revtxt :: !ByteString 
@@ -242,6 +274,23 @@ data Revision = Revision { _revmd5 :: !ByteString
               | RevisionInk { _revmd5 :: !ByteString
                             , _revink :: [Stroke] } 
               deriving Show 
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 1 } ''Revision )
+
+-- | 
+data Layer = Layer { layer_items :: ![Item] } 
+           deriving Show 
+
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 6 } ''Layer )
+
+-- | 
+data Page = Page { page_dim :: !Dimension
+                 , page_bkg :: !Background 
+                 , page_layers :: ![Layer] }
+          deriving Show 
+
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 5 } ''Page )
 
 -- | 
 data Hoodle = Hoodle { hoodle_id :: ByteString
@@ -252,15 +301,7 @@ data Hoodle = Hoodle { hoodle_id :: ByteString
                      , hoodle_pages :: ![Page] }
              deriving Show 
 
--- | 
-data Page = Page { page_dim :: !Dimension
-                 , page_bkg :: !Background 
-                 , page_layers :: ![Layer] }
-          deriving Show 
-
--- | 
-data Layer = Layer { layer_items :: ![Item] } 
-           deriving Show 
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 7 } ''Hoodle )
 
 -- | 
 getXYtuples :: Stroke -> [(Double,Double)]

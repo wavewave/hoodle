f2bf26aeb88bfe1d3510ba1d6ca214bddb5553dd
Ian-Woo Kim <ianwookim@gmail.com>
2016-06-14 13:33:49 -0700

    remove comments and show cairo_status. gave CAIRO_STATUS_NO_MEMORY.

--- pangotest/cairo.c
+++ pangotest/cairo.c
@@ -15,41 +15,15 @@ int main( int argc, char** argv )
   
   sfc = cairo_image_surface_create( CAIRO_FORMAT_ARGB32, 100, 100 ) ;
   cr = cairo_create( sfc );
-
-  //pctxt = pango_context_new();
   playout = pango_cairo_create_layout( cr );
-
   pfd = pango_font_description_new();
-  //playout = pango_layout_new( pctxt ) ;
-
   pango_layout_set_font_description( playout, pfd);
   pango_layout_set_markup( playout, "x<span size=\"0\">a<span letter_spacing=\"20000\">a</span>a</span>y", -1);
-
   pango_cairo_layout_path( cr, playout );
-
   pango_cairo_show_layout( cr, playout );
-
   status = cairo_surface_write_to_png( sfc, "text.png" ) ;
-  
   cairo_destroy( cr );
   cairo_surface_destroy( sfc ); 
   
   printf("status = %d\n", status);
 }
-
-/*
-
-cxt = Cairo::Context.new(Cairo::ImageSurface.new(100,100))
-layout = cxt.create_pango_layout
-font = Pango::FontDescription.new('Sans Bold 36')
-layout.font_description = font
-
-# This markup gives us space to render an image in the text flow and have it flow like words.
-# It is a bit hacky, but I can't figure out how to get ruby+pango+cairo to create arbitrary space
-layout.markup = 'x<span size="0">a<span letter_spacing="20000">a</span>a</span>y'
-
-cxt.pango_layout_path(layout) # fails here
-
-cxt.show_pango_layout layout
-cxt.target.write_to_png('bug_103.png')
-*/
--- pangotest/gtk.c
+++ pangotest/gtk.c
@@ -16,49 +16,31 @@ void draw_rectangle( cairo_t* cr )
 
 void draw_text( cairo_t* cr )
 {
-  //cairo_surface_t* sfc;
+
   PangoContext* pctxt;
   PangoLayout* playout;
   PangoFontDescription* pfd;
   int status;
-  
-  //sfc = cairo_image_surface_create( CAIRO_FORMAT_ARGB32, 100, 100 ) ;
 
-  //pctxt = pango_context_new();
   playout = pango_cairo_create_layout( cr );
-
   pfd = pango_font_description_new();
-  //playout = pango_layout_new( pctxt ) ;
-
   pango_layout_set_font_description( playout, pfd);
   pango_layout_set_markup( playout, "x<span size=\"0\">a<span letter_spacing=\"20000\">a</span>a</span>y", -1);
-
   pango_cairo_layout_path( cr, playout );
-
   pango_cairo_show_layout( cr, playout );
-
-  //status = cairo_surface_write_to_png( sfc, "text.png" ) ;
-  
-  //cairo_surface_destroy( sfc ); 
-  
-  //printf("status = %d\n", status);
+  status = cairo_status( cr );
+  printf("status = %d\n", status);
 }
 
 
 static gboolean
 on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
-  //cairo_t* cr;
-  //cr = gdk_cairo_create( widget->window );
-
   draw_rectangle( cr );
-  // draw_text( cr );
-
-  //cairo_destroy( cr );
+  draw_text( cr );
 
   return FALSE;
-
-  } 
+} 
 
 static void activate( GtkApplication *app, gpointer user_data )
 {
@@ -83,20 +65,6 @@ static void activate( GtkApplication *app, gpointer user_data )
   
   gtk_widget_show_all(window);
   
-  /*  // gtk_init( &argc, &argv );
-  g_signal_connect( window, "expose-event", G_CALLBACK(on_expose_event), NULL);
-
-  
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_default_size(GTK_WINDOW(window), 300, 230);
-  gtk_widget_set_app_paintable(window, TRUE);
-
-  gtk_widget_show_all(window);
-
-  gtk_main();
-
-  return 0; */
-  
 }
 
 int main( int argc, char* argv [] )
@@ -111,19 +79,3 @@ int main( int argc, char* argv [] )
   
 }
 
-/*
-
-cxt = Cairo::Context.new(Cairo::ImageSurface.new(100,100))
-layout = cxt.create_pango_layout
-font = Pango::FontDescription.new('Sans Bold 36')
-layout.font_description = font
-
-# This markup gives us space to render an image in the text flow and have it flow like words.
-# It is a bit hacky, but I can't figure out how to get ruby+pango+cairo to create arbitrary space
-layout.markup = 'x<span size="0">a<span letter_spacing="20000">a</span>a</span>y'
-
-cxt.pango_layout_path(layout) # fails here
-
-cxt.show_pango_layout layout
-cxt.target.write_to_png('bug_103.png')
-*/

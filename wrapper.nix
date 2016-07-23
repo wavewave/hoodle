098bb8ed79aa795413e68ac6cfa5ebdefb49c82f
Ian-Woo Kim <ianwookim@gmail.com>
2016-07-22 23:01:23 -0700

    correct icon theme in wrapper.nix

--- wrapper.nix
+++ wrapper.nix
@@ -8,17 +8,17 @@ in stdenv.mkDerivation {
      name = "hoodle-with-packages";
 
      nativeBuildInputs = [ makeWrapper ];
-     propagatedBuildInputs = [ wrapGAppsHook];
-     buildInputs = [ gnome3.defaultIconTheme
-                     gnome3.gsettings_desktop_schemas
-                   ] ;
+     #propagatedBuildInputs = [ wrapGAppsHook];
+     #buildInputs = [ gnome3.defaultIconTheme
+     #                gnome3.gsettings_desktop_schemas
+     #              ] ;
 
 
      buildCommand = ''
        mkdir -p $out/bin
        makeWrapper ${hoodleEnv}/bin/hoodle $out/bin/hoodle \
         --set NIX_GHC "${hoodleEnv}/bin/ghc" \
-        --set XDG_DATA_DIRS "${gtk3}/share/gsettings-schemas/${gtk3.name}:${gnome3.gsettings_desktop_schemas}/share/gsettings-schemas/${gnome3.gsettings_desktop_schemas.name}"
+        --set XDG_DATA_DIRS "${gtk3}/share/gsettings-schemas/${gtk3.name}:${gnome3.gsettings_desktop_schemas}/share/gsettings-schemas/${gnome3.gsettings_desktop_schemas.name}:${gnome3.defaultIconTheme}/share"
      '';
    }
 

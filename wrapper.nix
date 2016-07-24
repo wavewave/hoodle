13cc7f01cc5e9170612d119afaaf662eb2200341
Ian-Woo Kim <ianwookim@gmail.com>
2016-07-24 11:11:27 -0700

    correct gtk3 schema

--- wrapper.nix
+++ wrapper.nix
@@ -6,7 +6,7 @@ let hsconfig = import ./default.nix { poppler = poppler; gtk3 = gtk3; };
     hoodleEnv = newHaskellPackages.ghcWithPackages (p: with p; [ hoodle ] ++ packages p);
     #(p: with p; [
     #hoodle-parser hoodle-publish hoodle-render coroutine-object 
-    #Diff aeson-pretty case-insensitive configurator fsnotify handa-gdata hoodle-builder http-types pureMD5 system-filepath transformers-free websockets xournal-parser
+#Diff aeson-pretty case-insensitive configurator fsnotify handa-gdata hoodle-builder http-types pureMD5 system-filepath transformers-free websockets xournal-parser
 
 
 in stdenv.mkDerivation {
@@ -19,6 +19,6 @@ in stdenv.mkDerivation {
        mkdir -p $out/bin
        makeWrapper ${hoodleEnv}/bin/hoodle $out/bin/hoodle \
         --set NIX_GHC "${hoodleEnv}/bin/ghc" \
-        --set XDG_DATA_DIRS "${gtk3}/share/gsettings-schemas/${gtk3.name}:${gnome3.gsettings_desktop_schemas}/share/gsettings-schemas/${gnome3.gsettings_desktop_schemas.name}:${gnome3.defaultIconTheme}/share"
+        --set XDG_DATA_DIRS "${gtk3.out}/share/gsettings-schemas/${gtk3.name}:${gnome3.gsettings_desktop_schemas}/share/gsettings-schemas/${gnome3.gsettings_desktop_schemas.name}:${gnome3.defaultIconTheme}/share"
      '';
    }

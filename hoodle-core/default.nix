66409d50458798d21b29dde2a597360efd6e36be
Ian-Woo Kim <ianwookim@gmail.com>
2016-07-25 16:45:08 -0400

    hoodle-publish version up

--- hoodle-core/default.nix
+++ hoodle-core/default.nix
@@ -1,30 +1,28 @@
 { mkDerivation, aeson, aeson-pretty, array, attoparsec, base
-, base64-bytestring, binary, bytestring, cairo, case-insensitive
-, cereal, configurator, containers, coroutine-object, Diff
-, directory, either, errors, filepath, fsnotify, gd, gtk3
-, handa-gdata, hoodle-builder, hoodle-parser, hoodle-publish
-, hoodle-render, hoodle-types, http-types, lens, libX11, libXi
-, monad-loops, mtl, network-uri, pango, poppler, process, pureMD5
-, stdenv, stm, strict, svgcairo, system-filepath, template-haskell
-, text, time, transformers, transformers-free, unordered-containers
-, uuid, vector, websockets, xournal-parser, dyre, pkgconfig
+, base64-bytestring, binary, bytestring, cairo, cereal
+, configurator, containers, coroutine-object, Diff, directory
+, either, errors, filepath, fsnotify, gd, gtk3, hoodle-builder
+, hoodle-parser, hoodle-publish, hoodle-render, hoodle-types
+, http-types, lens, libX11, libXi, monad-loops, mtl, network-uri
+, pango, poppler, process, pureMD5, stdenv, stm, strict, svgcairo
+, system-filepath, template-haskell, text, time, transformers
+, transformers-free, unordered-containers, uuid, vector
+, xournal-parser, dyre, pkgconfig
 }:
 mkDerivation {
   pname = "hoodle-core";
-  version = "0.16";
+  version = "0.16.0";
   src = ./.;
   libraryHaskellDepends = [
     aeson aeson-pretty array attoparsec base base64-bytestring binary
-    bytestring cairo case-insensitive cereal configurator containers
-    coroutine-object Diff directory either errors filepath fsnotify gd
-    gtk3 handa-gdata hoodle-builder hoodle-parser hoodle-publish
-    hoodle-render hoodle-types http-types lens monad-loops mtl
-    network-uri pango poppler process pureMD5 stm strict svgcairo
-    system-filepath template-haskell text time transformers
-    transformers-free unordered-containers uuid vector websockets
-    xournal-parser dyre
+    bytestring cairo cereal configurator containers coroutine-object
+    Diff directory either errors filepath fsnotify gd gtk3
+    hoodle-builder hoodle-parser hoodle-publish hoodle-render
+    hoodle-types http-types lens monad-loops mtl network-uri pango
+    poppler process pureMD5 stm strict svgcairo system-filepath
+    template-haskell text time transformers transformers-free
+    unordered-containers uuid vector xournal-parser dyre
   ];
-  configureFlags = [ "-fdyre" ];
   librarySystemDepends = [ libX11 libXi ];
   buildDepends = [ pkgconfig ];
   homepage = "http://ianwookim.org/hoodle";

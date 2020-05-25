2b4e6f038139f8ef7ba39480ab8c36aaee00a638
Ian-Woo Kim <ianwookim@gmail.com>
2020-05-25 17:40:47 +0000

    get coordinate and draw rectangle.

--- hoodle-wasm/pen.js
+++ hoodle-wasm/pen.js
@@ -1,4 +1,3 @@
-var target = document.getElementById("box");
 
 function shoutPointerType(e) {
     switch(e.pointerType) {
@@ -14,23 +13,49 @@ function shoutPointerType(e) {
     }
 }
 
+function shoutPointerCoord(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    console.log("(x,y) = (" + x + " , " + y + ")");
+}
+
+function drawRectangle(cvs, ctxt, e) {
+    //var x = e.clientX;
+    //var y = e.clientY;
+    var rect = cvs.getBoundingClientRect();
+    var scaleX = canvas.width / rect.width;
+    var scaleY = canvas.height / rect.height;
+    var x = (e.clientX - rect.left)*scaleX;
+    var y = (e.clientY - rect.top)*scaleY;
+
+    ctxt.fillStyle = "#ff0000";
+    ctxt.fillRect(x,y,5,5);
+}
+
 function onPointerDown(e) {
     console.log("on pointerdown");
     shoutPointerType(e);
+    shoutPointerCoord(e);
+    drawRectangle(canvas,context,e);
 }
 
 function onPointerUp(e) {
     console.log("on pointerup");
     shoutPointerType(e);
+    shoutPointerCoord(e);
 }
 
 function onPointerMove(e) {
     //console.log("on pointermove");
     //shoutPointerType(e);
+    shoutPointerCoord(e);
 }
 
-target.addEventListener("pointerdown", onPointerDown);
-target.addEventListener("pointerup"  , onPointerUp);
-target.addEventListener("pointermove", onPointerMove);
+var canvas = document.getElementById("box");
+var context = canvas.getContext("2d");
+
+canvas.addEventListener("pointerdown", onPointerDown);
+canvas.addEventListener("pointerup"  , onPointerUp);
+canvas.addEventListener("pointermove", onPointerMove);
 
 console.log ("pen.js is loaded");

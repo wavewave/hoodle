991501d845fec8013108565883c828bb4d33f36f
Ian-Woo Kim <ianwookim@gmail.com>
2020-05-25 17:11:13 +0000

    event types.

--- hoodle-wasm/pen.js
+++ hoodle-wasm/pen.js
@@ -1,15 +1,32 @@
 var target = document.getElementById("box");
 
+function shoutPointerType(e) {
+    switch(e.pointerType) {
+    case "mouse":
+        console.log("it's a mouse event!");
+        break;
+    case "pen":
+        console.log("it's a pen event!");
+        break;
+    case "touch":
+        console.log("it's a touch event!");
+        break;
+    }
+}
+
 function onPointerDown(e) {
     console.log("on pointerdown");
+    shoutPointerType(e);
 }
 
 function onPointerUp(e) {
     console.log("on pointerup");
+    shoutPointerType(e);
 }
 
 function onPointerMove(e) {
-    console.log("on pointermove");
+    //console.log("on pointermove");
+    //shoutPointerType(e);
 }
 
 target.addEventListener("pointerdown", onPointerDown);

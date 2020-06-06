151b6bbee901222cdc9f2b115fcab9140fc5613b
Ian-Woo Kim <ianwookim@gmail.com>
2020-06-06 15:23:32 -0700

    start ghcjs.

--- hoodle-ghcjs/Main.hs
+++ hoodle-ghcjs/Main.hs
@@ -1,28 +1,5 @@
-{-# LANGUAGE InterruptibleFFI #-}
-
 module Main where
-
-import Asterius.Types (JSFunction(..))
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever, replicateM_, void)
-import Data.IORef (newIORef, readIORef, writeIORef)
-import Foreign.StablePtr (StablePtr)
-
-foreign import javascript "wrapper" makeHaskellCallback :: IO () -> IO JSFunction
-
-foreign import javascript "callback = $1" register :: JSFunction -> IO ()
-
-tester :: IO ()
-tester = do
-  ref <- newIORef (0::Int)
-  putStrLn "tester"
-  replicateM_ 10 $ do
-    threadDelay 100000
-    n <- readIORef ref
-    putStrLn $ "step: " ++ show n
-    writeIORef ref (n+1)
 
 main :: IO ()
 main = do
-  jsf <- makeHaskellCallback tester
-  register jsf
+  putStrLn "ghcjs started"

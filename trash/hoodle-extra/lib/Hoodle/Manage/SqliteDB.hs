751a18e2ad9a75e75130f005cf769ae7fb5548fc
Ian-Woo Kim <ianwookim@gmail.com>
2020-06-14 15:10:19 -0700

    remove trash directory.

--- trash/hoodle-extra/lib/Hoodle/Manage/SqliteDB.hs
+++
@@ -1,9 +0,0 @@
-module Hoodle.Manage.SqliteDB where 
-
-import           Control.Applicative
-import qualified Data.Text as T
-import           System.Directory
-import           System.FilePath
-
-defaultDBFile :: IO T.Text
-defaultDBFile = (T.pack . (</> ".hoodle.d" </> "hoodleiddb.dat")) <$> getHomeDirectory

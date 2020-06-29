d17b181ea562d8945a20d5339ad4e03edb4d2fb7
Ian-Woo Kim <ianwookim@gmail.com>
2020-06-28 22:54:49 -0700

    shell for build.

---
+++ config.nix
@@ -0,0 +1,84 @@
+{ deps, haskell, poppler, gtk3 }:
+
+self: super: {
+  #"poppler" = let p = self.callCabal2nix "poppler" deps.poppler { poppler = poppler; };
+  #            in haskell.lib.overrideCabal p (drv: {
+  #              configureFlags = ["-fgtk3"];
+  #              jailbreak = true;
+  #            });
+
+  "pdf-toolbox-core" = self.callCabal2nix "pdf-toolbox-core" (deps.pdf-toolbox + "/core") {};
+  "pdf-toolbox-content" = self.callCabal2nix "pdf-toolbox-content" (deps.pdf-toolbox + "/content") {};
+  "pdf-toolbox-document" = self.callCabal2nix "pdf-toolbox-document" (deps.pdf-toolbox + "/document") {};
+
+
+
+#haskell.lib.doJailbreak p ;
+   "poppler" =
+self.callPackage
+  ({ mkDerivation, array, base, bytestring, cairo, containers
+   , glib, gtk3, gtk3lib, gtk2hs-buildtools, mtl, pango, popplerlib
+   , stdenv
+   }:
+   mkDerivation {
+    pname = "poppler";
+    version = "0.15";
+    src = deps.poppler;
+
+    configureFlags = ["-fgtk3"];
+    hardeningDisable=["fortify"];
+    libraryHaskellDepends = [
+      array base bytestring cairo containers glib gtk3 mtl
+    ];
+    libraryPkgconfigDepends = [ gtk3lib pango popplerlib ];
+    libraryToolDepends = [ gtk2hs-buildtools ];
+    homepage = "http://projects.haskell.org/gtk2hs";
+    description = "Binding to the Poppler";
+    license = stdenv.lib.licenses.gpl2;
+  }) { popplerlib = poppler; gtk3lib = gtk3; };
+
+  coroutine-object = self.callPackage ./coroutine-object {};
+
+  xournal-types    = self.callPackage ./xournal-types {};
+
+  xournal-parser   = self.callPackage ./xournal-parser {
+                       xournal-types = self.xournal-types;
+                     };
+
+  hoodle-types     = self.callPackage ./hoodle-types {};
+
+  hoodle-builder   = self.callPackage ./hoodle-builder {
+                       hoodle-types = self.hoodle-types;
+                     };
+
+  hoodle-parser    = self.callPackage ./hoodle-parser {
+                       hoodle-types = self.hoodle-types;
+                       xournal-types = self.xournal-types;
+                     };
+
+  hoodle-render    = self.callPackage ./hoodle-render {
+                       hoodle-types = self.hoodle-types;
+                     };
+
+  hoodle-publish   = self.callPackage ./hoodle-publish {
+                       hoodle-types = self.hoodle-types;
+                       hoodle-render = self.hoodle-render;
+                     };
+
+  hoodle-core      = self.callPackage ./hoodle-core {
+                       coroutine-object = self.coroutine-object;
+                       hoodle-types = self.hoodle-types;
+                       hoodle-parser = self.hoodle-parser;
+                       hoodle-builder = self.hoodle-builder;
+                       hoodle-render = self.hoodle-render;
+                       hoodle-publish = self.hoodle-publish;
+                       xournal-parser = self.xournal-parser;
+                       gtk3 = gtk3;
+                     };
+  hoodle           = self.callPackage ./hoodle {
+                       hoodle-core = self.hoodle-core;
+                     };
+
+
+
+}
--- shell.nix
+++ shell.nix
@@ -1,24 +1,98 @@
-{ pkgs ? (import <nixpkgs>{}) }:
+{ pkgs ?
+import (builtins.fetchTarball {
+    name = "nixos-20.03";
+    url = "https://github.com/nixos/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  }) {}
+}:
 
-let hsconfig = import ./default.nix { poppler = pkgs.poppler; gtk3 = pkgs.gtk3; };
-    newhaskellpackages = pkgs.haskellPackages.override { overrides = hsconfig; };
-    hsenv = newhaskellpackages.ghcWithPackages 
-      (p: with p; [ hoodle-builder hoodle-render hoodle-publish xournal-parser
-                    hoodle-types xournal-types coroutine-object case-insensitive
+with pkgs;
+
+let
+    deps = callPackage ./deps.nix {};
+    hsconfig = import ./config.nix {
+      inherit deps;
+      inherit haskell;
+      poppler = pkgs.poppler;
+      gtk3 = pkgs.gtk3;
+    };
+    newHsPkgs = haskell.packages.ghc865.override { overrides = hsconfig; };
+
+    hoodlePkgs = [
+      "coroutine-object"
+      "xournal-types"
+      "xournal-parser"
+      "hoodle-types"
+      "hoodle-builder"
+      "hoodle-parser"
+      "hoodle-render"
+      "hoodle-phublish"
+      "hoodle-core"
+      "hoodle"
+    ];
+
+    pname = lib.attrByPath [ "pname" ] null;
+
+    removeHoodlePkgsFromDep =
+      ps: lib.filter (q: let n = pname q; in !(lib.any (k: k == n) hoodlePkgs)) ps;
+
+    hsenv = newHsPkgs.ghcWithPackages
+      (ps: with ps;
+        let deps = builtins.concatMap
+                     (p: p.buildInputs ++ p.nativeBuildInputs ++ p.propagatedBuildInputs)
+                     [ coroutine-object
+                       xournal-types
+                       xournal-parser
+                       hoodle-types
+                       hoodle-builder
+                       hoodle-parser
+                       hoodle-render
+                       hoodle-publish
+                       hoodle-core
+                       hoodle
+                     ];
+        in #removeHoodlePkgsFromDep deps
+           [ #gtk3
+             #cairo
+             #glib
+             poppler
+             #gtk2hs-buildtools
+  case-insensitive
                     attoparsec-conduit SHA aeson-pretty blaze-html hinotify zlib-conduit
-                    http-client-tls http-conduit xml fsnotify json pem pureMD5 
+                    http-client-tls http-conduit xml fsnotify json pem pureMD5
                     regex-base regex-posix configurator http-types system-filepath
-                    transformers-free websockets 
-                    data-default-instances-containers asn1-parse 
+                    transformers-free websockets
+                    data-default-instances-containers asn1-parse
                     Diff
-                  ]);
- in pkgs.stdenv.mkDerivation { 
-      name = "env-hoodle-build";
-      buildInputs = [ hsenv pkgs.haskellPackages.cabal-install pkgs.x11 pkgs.xlibs.libXi pkgs.gtk3 pkgs.poppler pkgs.pkgconfig
-                    ];
-      shellHook = ''
-      '';
+                    free strict microlens double-conversion uuid svgcairo monad-loops
+                    lens gd errors either
+                    #pdf-toolbox-core
+                    pdf-toolbox-document
+                    directory-tree
+                    HTTP xml-types xml-conduit TypeCompose
+           ]
+      );
 
-    }
+in
+
+mkShell {
+  buildInputs = [ hsenv cabal-install ];
+}
 
 
+ #     (p: with p; [ hoodle-builder hoodle-render hoodle-publish xournal-parser
+ #                   hoodle-types xournal-types coroutine-object case-insensitive
+ #                   attoparsec-conduit SHA aeson-pretty blaze-html hinotify zlib-conduit
+ #                   http-client-tls http-conduit xml fsnotify json pem pureMD5
+ #                   regex-base regex-posix configurator http-types system-filepath
+ #                   transformers-free websockets
+ #                   data-default-instances-containers asn1-parse
+ #                   Diff
+ #                 ]);
+ #in pkgs.stdenv.mkDerivation {
+ #     name = "env-hoodle-build";
+ #     buildInputs = [ hsenv pkgs.haskellPackages.cabal-install pkgs.x11 pkgs.xlibs.libXi pkgs.gtk3 pkgs.poppler pkgs.pkgconfig
+ #                   ];
+ #     shellHook = ''
+ #     '';
+ #   }

85669877cf87ceb1dfd78e7d41cc2b27a63bbee6
Ian-Woo Kim <ianwookim@gmail.com>
2020-06-29 00:38:25 -0700

    remove dead build code.

--- shell.nix
+++ shell.nix
@@ -18,58 +18,20 @@ let
     };
     newHsPkgs = haskell.packages.ghc865.override { overrides = hsconfig; };
 
-    hoodlePkgs = [
-      "coroutine-object"
-      "xournal-types"
-      "xournal-parser"
-      "hoodle-types"
-      "hoodle-builder"
-      "hoodle-parser"
-      "hoodle-render"
-      "hoodle-phublish"
-      "hoodle-core"
-      "hoodle"
-    ];
-
-    pname = lib.attrByPath [ "pname" ] null;
-
-    removeHoodlePkgsFromDep =
-      ps: lib.filter (q: let n = pname q; in !(lib.any (k: k == n) hoodlePkgs)) ps;
-
     hsenv = newHsPkgs.ghcWithPackages
       (ps: with ps;
-        let deps = builtins.concatMap
-                     (p: p.buildInputs ++ p.nativeBuildInputs ++ p.propagatedBuildInputs)
-                     [ coroutine-object
-                       xournal-types
-                       xournal-parser
-                       hoodle-types
-                       hoodle-builder
-                       hoodle-parser
-                       hoodle-render
-                       hoodle-publish
-                       hoodle-core
-                       hoodle
-                     ];
-        in #removeHoodlePkgsFromDep deps
-           [ #gtk3
-             #cairo
-             #glib
-             poppler
-             #gtk2hs-buildtools
-  case-insensitive
-                    attoparsec-conduit SHA aeson-pretty blaze-html hinotify zlib-conduit
-                    http-client-tls http-conduit xml fsnotify json pem pureMD5
-                    regex-base regex-posix configurator http-types system-filepath
-                    transformers-free websockets
-                    data-default-instances-containers asn1-parse
-                    Diff
-                    free strict microlens double-conversion uuid svgcairo monad-loops
-                    lens gd errors either
-                    #pdf-toolbox-core
-                    pdf-toolbox-document
-                    directory-tree
-                    HTTP xml-types xml-conduit TypeCompose
+           [ poppler
+             case-insensitive
+             attoparsec-conduit SHA aeson-pretty blaze-html hinotify zlib-conduit
+             http-client-tls http-conduit xml fsnotify json pem pureMD5
+             regex-base regex-posix configurator http-types system-filepath
+             transformers-free websockets
+             data-default-instances-containers asn1-parse Diff
+             free strict microlens double-conversion uuid svgcairo monad-loops
+             lens gd errors either
+             pdf-toolbox-document
+             directory-tree
+             HTTP xml-types xml-conduit TypeCompose
            ]
       );
 
@@ -78,21 +40,3 @@ in
 mkShell {
   buildInputs = [ hsenv cabal-install pkgconfig ];
 }
-
-
- #     (p: with p; [ hoodle-builder hoodle-render hoodle-publish xournal-parser
- #                   hoodle-types xournal-types coroutine-object case-insensitive
- #                   attoparsec-conduit SHA aeson-pretty blaze-html hinotify zlib-conduit
- #                   http-client-tls http-conduit xml fsnotify json pem pureMD5
- #                   regex-base regex-posix configurator http-types system-filepath
- #                   transformers-free websockets
- #                   data-default-instances-containers asn1-parse
- #                   Diff
- #                 ]);
- #in pkgs.stdenv.mkDerivation {
- #     name = "env-hoodle-build";
- #     buildInputs = [ hsenv pkgs.haskellPackages.cabal-install pkgs.x11 pkgs.xlibs.libXi pkgs.gtk3 pkgs.poppler pkgs.pkgconfig
- #                   ];
- #     shellHook = ''
- #     '';
- #   }

08cc37b0d810635feb5502313b5da8c80486c76c
Ian-Woo Kim <ianwookim@gmail.com>
2020-07-01 22:11:20 -0700

    XDG_DATA_DIRS in shell.nix.

--- shell.nix
+++ shell.nix
@@ -39,4 +39,7 @@ in
 
 mkShell {
   buildInputs = [ hsenv cabal-install pkgconfig ];
+  shellHook = ''
+    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
+  '';
 }

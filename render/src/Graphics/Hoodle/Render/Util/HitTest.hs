a09f5609521aad297c860ce4897bf7ccad91fd77
Ian-Woo Kim <ianwookim@gmail.com>
2020-07-04 21:15:26 +0000

    hoodle-types and hoodle-render now depend on hoodle-util. now sharing hit test functions. (#105)

--- render/src/Graphics/Hoodle/Render/Util/HitTest.hs
+++ render/src/Graphics/Hoodle/Render/Util/HitTest.hs
@@ -1,44 +1,60 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Graphics.Hoodle.Render.Util.HitTest where
+-- NOTE: this module will be deprecated.
 
-import Control.Applicative
-import Control.Monad.State
-import Data.Hoodle.BBox
+module Graphics.Hoodle.Render.Util.HitTest
+  ( isPointInBBox, -- re-export
+    do2LinesIntersect, -- re-export
+    do2BBoxIntersect, -- re-export
+    isBBox2InBBox1, -- re-export
+    hltHittedByBBox, -- re-export
+    hltEmbeddedByBBox, -- re-export
+    hltHittedByLineRough, -- re-export
+    elimHitted, -- re-export
+    merge, -- re-export
+    getTotalBBox, -- re-export
+    --
+    doesLineHitStrk,
+    hltFilteredBy_StateT,
+    hltFilteredBy,
+    hltItmsHittedByLine_StateT,
+    hltItmsHittedByLineFrmSelected_StateT,
+  )
+where
+
+import Control.Monad.State (State, evalState, modify)
+import Data.Hoodle.BBox (bbxed_content)
 import Data.Hoodle.Simple
-import Data.Strict.Tuple
+  ( Stroke (Stroke, VWStroke, stroke_data),
+  )
+import Data.Strict.Tuple (Pair ((:!:)))
 import Graphics.Hoodle.Render.Type.HitTest
+  ( AlterList ((:-), Empty),
+    RItemHitted,
+  )
 import Graphics.Hoodle.Render.Type.Item
+  ( RItem (RItemStroke),
+  )
+import Hoodle.HitTest
+  ( do2BBoxIntersect,
+    do2LinesIntersect,
+    elimHitted,
+    getTotalBBox,
+    hltEmbeddedByBBox,
+    hltHittedByBBox,
+    hltHittedByLineRough,
+    isBBox2InBBox1,
+    isPointInBBox,
+    merge,
+  )
+import Hoodle.HitTest.Type
+  ( Hitted (..),
+    NotHitted (..),
+  )
 
 --------------------------
 -- hit test collections --
 --------------------------
-
--- | hit test of whether a point in a bbox
---   previously, hitTestBBoxPoint
-isPointInBBox ::
-  BBox ->
-  -- | point
-  (Double, Double) ->
-  Bool
-isPointInBBox (BBox (ulx, uly) (lrx, lry)) (x, y) =
-  ulx <= x && x <= lrx && uly <= y && y <= lry
-
--- | hit test of whether two lines intersect
-do2LinesIntersect ::
-  -- | line1
-  ((Double, Double), (Double, Double)) ->
-  -- | line2
-  ((Double, Double), (Double, Double)) ->
-  Bool
-do2LinesIntersect ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4)) =
-  (x2 - xc) * (xc - x1) > 0 && (x3 - xc) * (xc - x4) > 0
-  where
-    x21 = x2 - x1
-    x43 = x4 - x3
-    y21 = y2 - y1
-    y43 = y4 - y3
-    xc = (x21 * x43 * (y3 - y1) + y21 * x43 * x1 - y43 * x21 * x3) / (y21 * x43 - y43 * x21)
 
 -- | previously, hitTestLineStroke
 doesLineHitStrk :: ((Double, Double), (Double, Double)) -> Stroke -> Bool
@@ -56,34 +72,6 @@ doesLineHitStrk line1 (VWStroke _t _c d) = test d
     test ((x0, y0, _) : (x, y, z) : rest) =
       do2LinesIntersect line1 ((x0, y0), (x, y))
         || test ((x, y, z) : rest)
-
--- | Do two bounding boxes intersect with each other?
---   previously, hitTestBBoxBBox
-do2BBoxIntersect :: BBox -> BBox -> Bool
-do2BBoxIntersect (BBox (ulx1, uly1) (lrx1, lry1)) (BBox (ulx2, uly2) (lrx2, lry2)) =
-  p1 && p2
-  where
-    p1 =
-      ulx1 <= ulx2 && ulx2 <= lrx1
-        || ulx1 <= lrx2 && lrx2 <= lrx1
-        || ulx2 <= ulx1 && ulx1 <= lrx2
-        || ulx2 <= lrx1 && lrx1 <= lrx2
-    p2 =
-      uly1 <= uly2 && uly2 <= lry1
-        || uly1 <= lry2 && lry2 <= lry1
-        || uly2 <= uly1 && uly1 <= lry2
-        || uly2 <= lry1 && lry1 <= lry2
-
--- | is the second bbox inside the first bbox?
---   previously, hitTestInsideBBox
-isBBox2InBBox1 ::
-  -- | 1st bbox
-  BBox ->
-  -- | 2nd bbox
-  BBox ->
-  Bool
-isBBox2InBBox1 b1 (BBox (ulx2, uly2) (lrx2, lry2)) =
-  isPointInBBox b1 (ulx2, uly2) && isPointInBBox b1 (lrx2, lry2)
 
 --------------------------------------------------------
 -- item filtering functions that results in AlterList --
@@ -115,43 +103,10 @@ hltFilteredBy ::
 hltFilteredBy test is = evalState (hltFilteredBy_StateT test is) False
 
 -- |
-hltHittedByBBox ::
-  (GetBBoxable a) =>
-  -- | test bounding box
-  BBox ->
-  -- | items to test
-  [a] ->
-  AlterList (NotHitted a) (Hitted a)
-hltHittedByBBox b = hltFilteredBy (do2BBoxIntersect b . getBBox)
-
--- |
-hltEmbeddedByBBox ::
-  (GetBBoxable a) =>
-  BBox ->
-  [a] ->
-  AlterList (NotHitted a) (Hitted a)
-hltEmbeddedByBBox b = hltFilteredBy (isBBox2InBBox1 b . getBBox)
-
--- | only check if a line and bbox of item overlapped
-hltHittedByLineRough ::
-  (GetBBoxable a) =>
-  -- | line
-  ((Double, Double), (Double, Double)) ->
-  -- | items to test
-  [a] ->
-  AlterList (NotHitted a) (Hitted a)
-hltHittedByLineRough (p1, p2) = hltFilteredBy boxhittest
-  where
-    boxhittest s =
-      isPointInBBox (getBBox s) p1
-        || isPointInBBox (getBBox s) p2
-
--- |
 hltItmsHittedByLine_StateT ::
   ((Double, Double), (Double, Double)) ->
   [RItem] ->
   State Bool RItemHitted
--- (AlterList (NotHitted RItem) (Hitted RItem))
 hltItmsHittedByLine_StateT line = hltFilteredBy_StateT test
   where
     test (RItemStroke strk) = (doesLineHitStrk line . bbxed_content) strk
@@ -160,7 +115,7 @@ hltItmsHittedByLine_StateT line = hltFilteredBy_StateT test
 -- |
 hltItmsHittedByLineFrmSelected_StateT ::
   ((Double, Double), (Double, Double)) ->
-  RItemHitted -> -- AlterList (NotHitted RItem) (Hitted RItem)
+  RItemHitted ->
   State Bool (AlterList (NotHitted RItem) RItemHitted)
 hltItmsHittedByLineFrmSelected_StateT _ Empty =
   error "something is wrong, invariant broken"
@@ -168,30 +123,3 @@ hltItmsHittedByLineFrmSelected_StateT _ (n :- Empty) = return (n :- Empty)
 hltItmsHittedByLineFrmSelected_StateT line (n :- h :- rest) = do
   h' <- hltItmsHittedByLine_StateT line (unHitted h)
   (n :-) . (h' :-) <$> hltItmsHittedByLineFrmSelected_StateT line rest
-
--- |
-elimHitted ::
-  (GetBBoxable a) =>
-  AlterList (NotHitted a) (Hitted a) ->
-  State (Maybe BBox) [a]
-elimHitted Empty = error "something wrong in elimHitted"
-elimHitted (n :- Empty) = return (unNotHitted n)
-elimHitted (n :- h :- rest) = do
-  bbox <- get
-  let bbox2 = getTotalBBox (unHitted h)
-  put (merge bbox bbox2)
-  return . (unNotHitted n ++) =<< elimHitted rest
-
--- |
-merge :: Maybe BBox -> Maybe BBox -> Maybe BBox
-merge Nothing Nothing = Nothing
-merge Nothing (Just b) = Just b
-merge (Just b) Nothing = Just b
-merge (Just (BBox (x1, y1) (x2, y2))) (Just (BBox (x3, y3) (x4, y4))) =
-  Just (BBox (min x1 x3, min y1 y3) (max x2 x4, max y2 y4))
-
--- |
-getTotalBBox :: (GetBBoxable a) => [a] -> Maybe BBox
-getTotalBBox = foldl f Nothing
-  where
-    f acc = merge acc . Just . getBBox

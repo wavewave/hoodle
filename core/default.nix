473da46a230d382d5359475f2a9d2004ae3bf913
Ian-Woo Kim <ianwookim@gmail.com>
2020-07-05 12:53:54 -0700

    update core/default.nix

--- core/default.nix
+++ core/default.nix
@@ -1,13 +1,14 @@
 { mkDerivation, aeson, aeson-pretty, array, attoparsec, base
 , base64-bytestring, binary, bytestring, cairo, cereal
 , configurator, containers, coroutine-object, Diff, directory
-, either, errors, filepath, fsnotify, gd, gtk3, hoodle-builder
+, errors, filepath, fsnotify, gd, gtk3, hoodle-builder
 , hoodle-parser, hoodle-publish, hoodle-render, hoodle-types
-, http-types, lens, libX11, libXi, monad-loops, mtl, network-uri
-, pango, poppler, process, pureMD5, stdenv, stm, strict, svgcairo
-, system-filepath, template-haskell, text, time, transformers
-, transformers-free, unordered-containers, uuid, vector
-, xournal-parser, dyre, pkgconfig
+, hoodle-util, http-types, lens, libX11, libXi, monad-loops, mtl
+, network, network-info, network-simple, network-uri, pango
+, poppler, process, pureMD5, resourcet, stdenv, stm, strict
+, svgcairo, system-filepath, template-haskell, text, time
+, transformers, transformers-free, unordered-containers, uuid
+, vector, xournal-parser
 }:
 mkDerivation {
   pname = "hoodle-core";
@@ -16,15 +17,14 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson aeson-pretty array attoparsec base base64-bytestring binary
     bytestring cairo cereal configurator containers coroutine-object
-    Diff directory either errors filepath fsnotify gd gtk3
-    hoodle-builder hoodle-parser hoodle-publish hoodle-render
-    hoodle-types http-types lens monad-loops mtl network-uri pango
-    poppler process pureMD5 stm strict svgcairo system-filepath
-    template-haskell text time transformers transformers-free
-    unordered-containers uuid vector xournal-parser dyre
+    Diff directory errors filepath fsnotify gd gtk3 hoodle-builder
+    hoodle-parser hoodle-publish hoodle-render hoodle-types hoodle-util
+    http-types lens monad-loops mtl network network-info network-simple
+    network-uri pango poppler process pureMD5 resourcet stm strict
+    svgcairo system-filepath template-haskell text time transformers
+    transformers-free unordered-containers uuid vector xournal-parser
   ];
   librarySystemDepends = [ libX11 libXi ];
-  buildDepends = [ pkgconfig ];
   homepage = "http://ianwookim.org/hoodle";
   description = "Core library for hoodle";
   license = stdenv.lib.licenses.bsd3;

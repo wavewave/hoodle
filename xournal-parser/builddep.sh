f354be052842fc24277bbc86dd05e8114fb406be
Ian-Woo Kim <ianwookim@gmail.com>
2020-07-05 23:53:48 -0700

    use callCabal2nix. no more redundant default.nix

--- xournal-parser/builddep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-mkdir deps
-git clone https://github.com/wavewave/devadmin.git deps/devadmin
-cd deps/devadmin ; cabal install ; cd ../../
-$HOME/.cabal/bin/build cloneall --config=build.conf
-$HOME/.cabal/bin/build bootstrap --config=build.conf
-

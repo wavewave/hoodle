f405f8bee8f03ba547d84bc7b64b63e0af164a82
Ian-Woo Kim <ianwookim@gmail.com>
2020-08-02 20:42:24 -0700

    hoodle-static nix package now has static html/js and ghcjs jsexe.

--- web/release.nix
+++ web/release.nix
@@ -11,12 +11,38 @@ let
     };
   };
 
-  hoodle-static = ./static;
+  newHsPkgs_ghcjs = haskell.packages.ghcjs.override {
+    overrides = self: super: {
+      coroutine-object = self.callCabal2nix "coroutine-object" ../coroutine-object {};
+      hoodle-util = self.callCabal2nix "hoodle-util" ../util {};
+      comonad = haskell.lib.dontCheck super.comonad;
+      semigroupoids = haskell.lib.dontCheck super.semigroupoids;
+      QuickCheck = haskell.lib.dontCheck super.QuickCheck;
+      scientific = haskell.lib.dontCheck super.scientific;
+      tasty-quickcheck = haskell.lib.dontCheck super.tasty-quickcheck;
+      time-compat = haskell.lib.dontCheck super.time-compat;
+      hoodle-web-common = self.callCabal2nix "hoodle-web-common" ./common {};
+      hoodle-web = self.callCabal2nix "hoodle-web" ./client {};
+    };
+  };
+
+  hoodle-static = stdenv.mkDerivation {
+    name = "hoodle-static";
+    src = ./static;
+    phases = [ "unpackPhase" "installPhase" ];
+    installPhase = ''
+      mkdir -p $out
+      cp hoodle.html pen.js svg.min.js svg.min.js.map $out
+      cp -a ${newHsPkgs_ghcjs.hoodle-web}/bin/hoodle-web.jsexe $out
+    '';
+  };
 
 in
 
 with newHsPkgs;
 {
-  inherit hoodle-server hoodle-web-common;
+  hoodle-server = newHsPkgs.hoodle-server;
   inherit hoodle-static;
+  hoodle-web = newHsPkgs_ghcjs.hoodle-web;
+
 }

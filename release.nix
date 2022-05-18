f1692cd26a7302f60cc1eba19514a7115e8c80bb
Ian-Woo Kim <ianwookim@gmail.com>
2022-05-18 14:18:48 -0700

    start build reform.

--- release.nix
+++ release.nix
@@ -3,18 +3,51 @@
 with pkgs;
 
 let
-    deps = callPackage ./deps.nix {};
-    hsconfig = import ./config.nix {
-      inherit deps;
-      inherit haskell;
-      poppler = pkgs.poppler;
-      gtk3 = pkgs.gtk3;
-    };
-    newHsPkgs = haskell.packages.ghc865.override { overrides = hsconfig; };
+  deps = callPackage ./deps.nix { };
+  hsconfig = callPackage ./config.nix {
+    inherit deps;
+    inherit haskell;
+    #poppler = pkgs.poppler_gi;
+    gtk3 = pkgs.gtk3;
+    #inherit makeWrapper shared-mime-info gsettings-desktop-schemas;
+  };
+  newHsPkgs = haskell.packages.ghc8107.override { overrides = hsconfig; };
+  #hoodleWrapper = stdenv.mkDerivation {
+  #  pname = "hoodle-wrapper";
+  #  version = "1.0";
+  #  nativeBuildInputs = [ makeWrapper ];
+  #  buildInputs = [ newHsPkgs.hoodle ];
+  #  installPhase = ''
+  #    mkdir -p $out/bin
+  #    makeWrapper ${newHsPkgs.hoodle}/bin/hoodle $out/bin/hoodle
+  #  '';
+  #};
+  # hoodleWrapper = mkbuildEnv {
+  #   name = "hoodle-wrapper";
+  #   buildInputs = [ pkgs.shared-mime-info ];
+  #   paths = [ newHsPkgs.hoodle ]; # pkgs.shared-mime-info ];
+  #   #pathsToLink =
+  #   #  [ "/share/applications" "/share/mime" "/share/menus" "/share/icons" ];
+  #   postBuild = ''
+  #     mkdir -p $out/share
+  #     cp -a ${pkgs.shared-mime-info}/share/* $out/share
 
-in
+  #     export XDG_DATA_DIRS=$out/share
+  #     chmod -R u+w $out/share/mime
+  #     update-mime-database -V $out/share/mime
+  #     #if [ -w $out/share/applications ]; then
+  #     #  update-desktop-database $out/share/applications
+  #     #fi
 
-with newHsPkgs;
-{
-  inherit microlens coroutine-object xournal-types xournal-parser hoodle-types hoodle-builder hoodle-parser hoodle-render hoodle-publish hoodle-core hoodle;
+  #     #if [ -w $out/share/mime ] \
+  #     #   && [ -w $out/share/mime/packages ]; then
+  #     #  
+  #     #fi
+  #  '';
+  #};
+
+in with newHsPkgs; {
+  inherit microlens coroutine-object xournal-types xournal-parser hoodle-types
+    hoodle-builder hoodle-parser hoodle-render hoodle-publish hoodle-core
+    hoodle;
 }

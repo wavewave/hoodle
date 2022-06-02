0036d42af8cc893609f5928663c27484aa103faf
Ian-Woo Kim <ianwookim@gmail.com>
2022-06-02 13:14:31 -0700

    proper handling of language extensions in checkImports

--- build-tools/app/checkImports.hs
+++ build-tools/app/checkImports.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Werror -Wall #-}
@@ -11,6 +12,9 @@ import Control.Monad (unless)
 import Control.Monad.IO.Class (liftIO)
 import Data.Foldable (for_)
 import DynFlags (DynFlags)
+import qualified DynFlags
+import qualified Extensions.Module as EM
+import qualified Extensions.Types as E
 import FastString (mkFastString)
 import qualified GHC
 import qualified GHC.Paths as GHC.Paths
@@ -20,7 +24,7 @@ import HsSyn (HsModule (..))
 import Lexer (P (unP), ParseResult (..), mkPState)
 import qualified Options.Applicative as OA
 import Outputable (ppr, showSDoc)
-import qualified Parser (parseModule)
+import qualified Parser
 import SrcLoc (mkRealSrcLoc, unLoc)
 import StringBuffer (stringToStringBuffer)
 import System.Exit (exitFailure)
@@ -52,22 +56,35 @@ main = do
   let srcfile = sourceFileName opts
   putStrLn $ "checking " <> srcfile
   contents <- readFile srcfile
-  GHC.runGhc (Just GHC.Paths.libdir) $ do
-    _env <- GHC.getSession
-    dflags <- GHC.getSessionDynFlags
-    let presult = runParser dflags contents srcfile Parser.parseModule
-    case presult of
-      POk _ r -> liftIO $ do
-        let modu :: HsModule GhcPs
-            modu = unLoc r
-            implicitImports = filter (isImplicitImport . unLoc) $ hsmodImports modu
-        unless (null implicitImports) $ do
-          putStrLn "implicit imports"
-          putStrLn "----------------"
-          for_ implicitImports $ \imp ->
-            putStrLn $ showSDoc dflags (ppr imp)
-          putStrLn "----------------"
-          exitFailure
-      _ -> do
-        liftIO $ putStrLn "Parse Failed"
-        liftIO exitFailure
+
+  eParsedExtensions <- EM.parseFile srcfile
+  case eParsedExtensions of
+    Left err -> print err
+    Right pexts -> do
+      let exts = E.parsedExtensionsAll pexts
+      GHC.runGhc (Just GHC.Paths.libdir) $ do
+        _env <- GHC.getSession
+        dflags <- GHC.getSessionDynFlags
+        let dflagsWithExts =
+              let addExts =
+                    foldr (.) id $
+                      fmap
+                        (\case E.On e -> flip DynFlags.xopt_set e; E.Off e -> flip DynFlags.xopt_unset e)
+                        exts
+               in addExts dflags
+        let presult = runParser dflagsWithExts contents srcfile Parser.parseModule
+        case presult of
+          POk _ r -> liftIO $ do
+            let modu :: HsModule GhcPs
+                modu = unLoc r
+                implicitImports = filter (isImplicitImport . unLoc) $ hsmodImports modu
+            unless (null implicitImports) $ do
+              putStrLn "implicit imports"
+              putStrLn "----------------"
+              for_ implicitImports $ \imp ->
+                putStrLn $ showSDoc dflags (ppr imp)
+              putStrLn "----------------"
+              exitFailure
+          _ -> do
+            liftIO $ putStrLn "Parse Failed"
+            liftIO exitFailure

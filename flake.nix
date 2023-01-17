3c4c289bc07847091bb15b30e30145b4549d344e
Ian-Woo Kim <ianwookim@gmail.com>
2023-01-17 00:22:32 -0800

    yet-failing trial of ghcjs shell build

--- flake.nix
+++ flake.nix
@@ -2,21 +2,29 @@
   description = "Hoodle: pen notetaking program";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
+    # hoodle-web is not buildable on the current master.
+    nixpkgs_20_03.url = "github:NixOS/nixpkgs/nixos-20.03";
     flake-utils.url = "github:numtide/flake-utils";
     TypeCompose = {
       url = "github:conal/TypeCompose/master";
       flake = false;
     };
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs_20_03, flake-utils, ... }:
     flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        pkgs_20_03 = import nixpkgs_20_03 {
+          inherit system;
+          config.allowBroken = true;
+        };
         haskellLib = pkgs.haskell.lib;
 
         parseCabalProject = import ./parse-cabal-project.nix;
         hoodlePackages = parseCabalProject ./cabal.project;
         hoodlePackageNames = builtins.map ({ name, ... }: name) hoodlePackages;
+
+        # gtk
         haskellOverlay = hself: hsuper:
           {
             "TypeCompose" =
@@ -45,9 +53,55 @@
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_ICON_DIRS:$XDG_DATA_DIRS
             '';
           };
-
         supportedCompilers = [ "ghc925" ];
         defaultCompiler = "ghc925";
+
+        # web
+        haskellOverlayWebClient = hself: hsuper: {
+          coroutine-object =
+            hself.callCabal2nix "coroutine-object" ./coroutine-object { };
+          hoodle-util = hself.callCabal2nix "hoodle-util" ./util { };
+          comonad = haskellLib.dontCheck hsuper.comonad;
+          semigroupoids = haskellLib.dontCheck hsuper.semigroupoids;
+          QuickCheck = haskellLib.dontCheck hsuper.QuickCheck;
+          scientific = haskellLib.dontCheck hsuper.scientific;
+          tasty-quickcheck = haskellLib.dontCheck hsuper.tasty-quickcheck;
+          time-compat = haskellLib.dontCheck hsuper.time-compat;
+        };
+        hpkgsWebClient =
+          pkgs_20_03.haskell.packages.ghcjs.extend haskellOverlayWebClient;
+
+        mkWebShellFor = compiler:
+          let
+            hsenvWebServer =
+              pkgs_20_03.haskell.packages.${compiler}.ghcWithPackages (p: [
+                p.acid-state
+                p.microlens
+                p.microlens-th
+                p.monad-loops
+                p.servant
+                p.servant-server
+                p.websockets
+              ]);
+            hsenvWebClient = (hpkgsWebClient).ghcWithPackages (p: [
+              p.coroutine-object
+              p.ghcjs-base
+              p.ghcjs-dom
+              #p.hoodle-util
+              p.microlens
+              p.microlens-th
+            ]);
+          in pkgs_20_03.mkShell {
+            name = "hoodle-web-shell";
+            buildInputs = [ # hsenvWebServer
+              hsenvWebClient
+              pkgs.cabal-install
+              pkgs.ormolu
+            ];
+          };
+
+        supportedCompilersWeb = [ "ghc865" ];
+        defaultCompilerWeb = "ghc865";
 
       in rec {
         # This package set is only useful for CI build test.
@@ -59,9 +113,15 @@
         inherit haskellOverlay;
 
         devShells = let
-          shells = pkgs.lib.genAttrs supportedCompilers
+          gtkShells = pkgs.lib.genAttrs supportedCompilers
             (compiler: mkShellFor compiler);
-        in { default = shells.${defaultCompiler}; } // shells;
+          webShells = pkgs.lib.genAttrs supportedCompilersWeb
+            (compiler: mkWebShellFor compiler);
+        in rec {
+          default = gtk.${defaultCompiler};
+          gtk = gtkShells;
+          web = webShells;
+        };
 
       });
 }

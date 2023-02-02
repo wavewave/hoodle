281fedf4e5e35f663afd6e47114ed1a902a77483
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-02 14:54:10 -0800

    eventlog test

--- hoodle/app/hoodle/Main.hs
+++ hoodle/app/hoodle/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -w #-}
+
 -- |
 -- Module      : Main
 -- Copyright   : (c) 2011-2013 Ian-Woo Kim
@@ -8,8 +10,17 @@
 -- Portability : GHC
 module Main where
 
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever)
+import GHC.Eventlog.Socket (flush, startWait)
 import Hoodle.Script (defaultScriptConfig)
 import Hoodle.StartUp (hoodleStartMain)
 
 main :: IO ()
-main = hoodleStartMain defaultScriptConfig
+main = do
+  startWait "/tmp/eventlog.sock"
+  {- forkIO $
+    forever $ do
+      threadDelay 1000000
+      flush -}
+  hoodleStartMain defaultScriptConfig

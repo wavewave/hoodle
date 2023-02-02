ca39a7eb9f2fb6106e3ae005202615082c16f0ac
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-02 15:42:26 -0800

    eventlog parsed with ghc-events.

--- log/app/logcat/Main.hs
+++ log/app/logcat/Main.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -w #-}
 
 module Main where
 
 import qualified Control.Exception as E
 import Control.Monad (forever)
+import qualified Data.ByteString.Lazy as BL
+import GHC.RTS.Events.Incremental (readEvents, readHeader)
 import Network.Socket
   ( Family (AF_UNIX),
     SockAddr (SockAddrUnix),
+    Socket,
     SocketType (Stream),
     close,
     connect,
@@ -14,6 +18,28 @@ import Network.Socket
     withSocketsDo,
   )
 import Network.Socket.ByteString (recv)
+import Text.Pretty.Simple (pPrint)
+
+dump :: Socket -> IO ()
+dump sock = goHeader ""
+  where
+    goHeader bs0 = do
+      bs1 <- recv sock 1024
+      let bs = bs0 <> bs1
+      let lbs = BL.fromStrict bs
+      let e = readHeader lbs
+      case e of
+        Left err -> print err >> goHeader bs
+        Right (hdr, lbs') -> pPrint hdr >> goEvents hdr (BL.toStrict lbs')
+
+    goEvents hdr bs0 = do
+      bs1 <- recv sock 100000000
+      let bs = bs0 <> bs1
+      let lbs = BL.fromStrict bs
+      let (evs, merr) = readEvents hdr lbs
+      mapM_ pPrint evs
+      print merr
+      goEvents hdr ""
 
 main :: IO ()
 main = do
@@ -23,7 +49,5 @@ main = do
           sock <- socket AF_UNIX Stream 0
           connect sock (SockAddrUnix file)
           pure sock
-    E.bracket open close $ \sock -> forever $ do
-      _ <- recv sock 1024
-      putStrLn "i got it!"
+    E.bracket open close dump
     pure ()

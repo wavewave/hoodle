35494ab7d27d06baf2e4956df6b7d566a0fb1078
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-02 15:08:26 -0800

    socket read

---
+++ log/logcat.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import qualified Control.Exception as E
+import Control.Monad (forever)
+import Network.Socket
+  ( Family (AF_UNIX),
+    SockAddr (SockAddrUnix),
+    SocketType (Stream),
+    close,
+    connect,
+    socket,
+    withSocketsDo,
+  )
+import Network.Socket.ByteString (recv)
+
+main :: IO ()
+main = do
+  withSocketsDo $ do
+    let file = "/tmp/eventlog.sock"
+        open = do
+          sock <- socket AF_UNIX Stream 0
+          connect sock (SockAddrUnix file)
+          pure sock
+    E.bracket open close $ \sock -> forever $ do
+      _ <- recv sock 1024
+      putStrLn "i got it!"
+    pure ()

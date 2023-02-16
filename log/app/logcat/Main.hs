f6d55d7bb435b2e645393ad1235c5b59c06062f2
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-16 15:25:01 -0800

    show event types histo

--- log/app/logcat/Main.hs
+++ log/app/logcat/Main.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -w #-}
 
 module Main where
 
 import qualified Control.Exception as E
 import Control.Monad (forever)
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import Data.List (group, sort)
+import Data.Maybe (mapMaybe)
+import GHC.Generics
 import GHC.RTS.Events.Incremental (readEvents, readHeader)
 import Network.Socket
   ( Family (AF_UNIX),
@@ -19,6 +25,124 @@ import Network.Socket
   )
 import Network.Socket.ByteString (recv)
 import Text.Pretty.Simple (pPrint)
+import GHC.RTS.Events (Event (..), EventInfo (..))
+
+histo :: [String] -> [(String, Int)]
+histo = mapMaybe count . group . sort
+  where
+    count ys@(x:xs) = Just (x, length ys)
+    count [] = Nothing
+
+eventInfoToString :: EventInfo -> String
+eventInfoToString info =
+  case info of
+    EventBlock {} -> "EventBlock"
+    UnknownEvent {} -> "UnknownEvent"
+    Startup {} -> "Startup"
+    Shutdown {} -> "Shutdown"
+    CreateThread {} -> "CreateThread"
+    RunThread {} -> "RunThread"
+    StopThread {} -> "StopThread"
+    ThreadRunnable {} -> "ThreadRunnable"
+    MigrateThread {} -> "MigrateThread"
+    WakeupThread {} -> "WakeupThread"
+    ThreadLabel {} -> "ThreadLabel"
+    CreateSparkThread {} -> "CreateSparkThread"
+    SparkCounters {} -> "SparkCounters"
+    SparkCreate {} -> "SparkCreate"
+    SparkDud {} -> "SparkDud"
+    SparkOverflow {} -> "SparkOverflow"
+    SparkRun {} -> "SparkRun"
+    SparkSteal {} -> "SparkSteal"
+    SparkFizzle {} -> "SparkFizzle"
+    SparkGC {} -> "SparkGC"
+    TaskCreate {} -> "TaskCreate"
+    TaskMigrate {} -> "TaskMigrate"
+    TaskDelete {} -> "TaskDelete"
+    RequestSeqGC {} -> "RequestSeqGC"
+    RequestParGC {} -> "RequestParGC"
+    StartGC {} -> "StartGC"
+    GCWork {} -> "GCWork"
+    GCIdle {} -> "GCIdle"
+    GCDone {} -> "GCDone"
+    EndGC {} -> "EndGC"
+    GlobalSyncGC {} -> "GlobalSyncGC"
+    GCStatsGHC {} -> "GCStatsGHC"
+    MemReturn {} -> "MemReturn"
+    HeapAllocated {} -> "HeapAllocated"
+    HeapSize {} -> "HeapSize"
+    BlocksSize {} -> "BlocksSize"
+    HeapLive {} -> "HeapLive"
+    HeapInfoGHC {} -> "HeapInfoGHC"
+    CapCreate {} -> "CapCreate"
+    CapDelete {} -> "CapDelete"
+    CapDisable {} -> "CapDisable"
+    CapEnable {} -> "CapEnable"
+    CapsetCreate {} -> "CapsetCreate"
+    CapsetDelete {} -> "CapsetDelete"
+    CapsetAssignCap {} -> "CapsetAssignCap"
+    CapsetRemoveCap {} -> "CapsetRemoveCap"
+    RtsIdentifier {} -> "RtsIdentifier"
+    ProgramArgs {} -> "ProgramArgs"
+    ProgramEnv {} -> "ProgramEnv"
+    OsProcessPid {} -> "OsProcessPid"
+    OsProcessParentPid {} -> "OsProcessParentPid"
+    WallClockTime {} -> "WallClockTime"
+    Message {} -> "Message"
+    UserMessage {} -> "UserMessage"
+    UserMarker {} -> "UserMarker"
+    Version {} -> "Version"
+    ProgramInvocation {} -> "ProgramInvocation"
+    CreateMachine {} -> "CreateMachine"
+    KillMachine {} -> "KillMachine"
+    CreateProcess {} -> "CreateProcess"
+    KillProcess {} -> "KillProcess"
+    AssignThreadToProcess {} -> "AssignThreadToProcess"
+    EdenStartReceive {} -> "EdenStartReceive"
+    EdenEndReceive {} -> "EdenEndReceive"
+    SendMessage {} -> "SendMessage"
+    ReceiveMessage {} -> "ReceiveMessage"
+    SendReceiveLocalMessage {} -> "SendReceiveLocalMessage"
+    InternString {} -> "InternString"
+    MerStartParConjunction {} -> "MerStartParConjunction"
+    MerEndParConjunction {} -> "MerEndParConjunction"
+    MerEndParConjunct {} -> "MerEndParConjunct"
+    MerCreateSpark {} -> "MerCreateSpark"
+    MerFutureCreate {} -> "MerFutureCreate"
+    MerFutureWaitNosuspend {} -> "MerFutureWaitNosuspend"
+    MerFutureWaitSuspended {} -> "MerFutureWaitSuspended"
+    MerFutureSignal {} -> "MerFutureSignal"
+    MerLookingForGlobalThread {} -> "MerLookingForGlobalThread"
+    MerWorkStealing {} -> "MerWorkStealing"
+    MerLookingForLocalSpark {} -> "MerLookingForLocalSpark"
+    MerReleaseThread {} -> "MerReleaseThread"
+    MerCapSleeping {} -> "MerCapSleeping"
+    MerCallingMain {} -> "MerCallingMain"
+    PerfName {} -> "PerfName"
+    PerfCounter {} -> "PerfCounter"
+    PerfTracepoint {} -> "PerfTracepoint"
+    HeapProfBegin {} -> "HeapProfBegin"
+    HeapProfCostCentre {} -> "HeapProfCostCentre"
+    InfoTableProv {} -> "InfoTableProv"
+    HeapProfSampleBegin {} -> "HeapProfSampleBegin"
+    HeapProfSampleEnd {} -> "HeapProfSampleEnd"
+    HeapBioProfSampleBegin {} -> "HeapBioProfSampleBegin"
+    HeapProfSampleCostCentre {} -> "HeapBioProfSampleCostCentre"
+    HeapProfSampleString {} -> "HeapProfSampleString"
+    ProfSampleCostCentre {} -> "ProfSampleCostCentre"
+    ProfBegin {} -> "ProfBegin"
+    UserBinaryMessage {} -> "UserBinaryMessage"
+    ConcMarkBegin {} -> "ConcMarkBegin"
+    ConcMarkEnd {} -> "ConcMarkEnd"
+    ConcSyncBegin {} -> "ConcSyncBegin"
+    ConcSyncEnd {} -> "ConcSyncEnd"
+    ConcSweepBegin {} -> "ConcSweepBegin"
+    ConcSweepEnd {} -> "ConcSweepEnd"
+    ConcUpdRemSetFlush {} -> "ConcUpdRemSetFlush"
+    NonmovingHeapCensus {} -> "NonmovingHeapCensus"
+    TickyCounterDef {} -> "TickyCounterDef"
+    TickyCounterSample {} -> "TickyCounterSample"
+    TickyBeginSample {} -> "TickyBeginSample"
 
 dump :: Socket -> IO ()
 dump sock = goHeader ""
@@ -35,9 +159,12 @@ dump sock = goHeader ""
     goEvents hdr bs0 = do
       bs1 <- recv sock 100000000
       let bs = bs0 <> bs1
+      print (BS.length bs)
       let lbs = BL.fromStrict bs
       let (evs, merr) = readEvents hdr lbs
-      mapM_ pPrint evs
+      putStrLn $ "number of events: " <> show (length evs)
+      print (histo $ fmap (eventInfoToString . evSpec) evs)
+      -- mapM_ pPrint evs
       print merr
       goEvents hdr ""
 

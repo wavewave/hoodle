3c83c78eeaedb2958f68d03d9e04f8320d589d93
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-22 10:59:19 -0800

    Refactor out nix expressions into separate files (#147)
    * separate out gtk.nix and web.nix
    
    * ghc_nix nix expressions is separated out.

---
+++ nix/ghc_nix.nix
@@ -0,0 +1,31 @@
+{
+  ghc_nix,
+  system,
+  pkgs,
+}: {
+  # ghc.nix shell
+  ghcNixShell = ghc_nix.outputs.devShells.${system}.default.overrideAttrs (attrs: {
+    buildInputs =
+      attrs.buildInputs
+      ++ [
+        pkgs.epoxy.dev
+        pkgs.gd
+        pkgs.gtk3
+        pkgs.libdatrie
+        pkgs.libdeflate
+        pkgs.librsvg.dev
+        pkgs.libthai
+        pkgs.pcre
+        pkgs.pcre2
+        pkgs.xorg.libXdmcp.dev
+        pkgs.libxkbcommon.dev
+        pkgs.xorg.libXtst
+        pkgs.pkgconfig
+      ]
+      ++ pkgs.lib.optional pkgs.stdenv.isLinux [pkgs.libselinux pkgs.libsepol pkgs.util-linux.dev];
+
+    shellHook = ''
+      export PS1="\n[hoodle-ghc.nix:\w]$ \0"
+    '';
+  });
+}

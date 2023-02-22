74e90bc61546c304568aee39dbab7ce1f1c4182d
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-21 18:53:55 -0800

    eventlog conditionally

--- hoodle/app/hoodle/Main.hs
+++ hoodle/app/hoodle/Main.hs
@@ -1,14 +1,7 @@
 {-# OPTIONS_GHC -w #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE NumericUnderscores #-}
 
--- |
--- Module      : Main
--- Copyright   : (c) 2011-2013 Ian-Woo Kim
---
--- License     : GPL-3
--- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
--- Stability   : experimental
--- Portability : GHC
 module Main where
 
 import Control.Concurrent (forkIO, threadDelay)
@@ -20,9 +13,11 @@ import Hoodle.StartUp (hoodleStartMain)
 
 main :: IO ()
 main = do
+#ifdef USE_EVENTLOG
   startWait "/tmp/eventlog.sock"
   forkIO $
     forever $ do
       threadDelay 5_000_000
       flushEventLog
+#endif
   hoodleStartMain defaultScriptConfig

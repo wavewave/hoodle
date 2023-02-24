69395484583d789c3dd1afe3a5058a166de839c0
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-24 09:41:15 -0800

    update canvas with animation

--- log/app/logcat/Main.hs
+++ log/app/logcat/Main.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -w #-}
 
 module Main where
 
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import qualified Control.Exception as E
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
-import Data.GI.Base (new, on, AttrOp ((:=)))
+import Data.GI.Base (AttrOp ((:=)), new, on)
 import Data.GI.Base.ManagedPtr (withManagedPtr)
+import Data.GI.Gtk.Threading (postGUIASync)
 import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
 import Data.List (group, sort)
 import Data.Maybe (fromMaybe, mapMaybe)
@@ -26,7 +28,7 @@ import GHC.RTS.Events.Incremental
   )
 import qualified GI.Cairo.Render as R
 import GI.Cairo.Render.Connector (renderWithContext)
-import qualified GI.Gtk as GI
+import qualified GI.Gtk as Gtk
 import Network.Socket
   ( Family (AF_UNIX),
     SockAddr (SockAddrUnix),
@@ -40,127 +42,13 @@ import Network.Socket
 import Network.Socket.ByteString (recv)
 import System.IO (hFlush, stdout)
 import Text.Pretty.Simple (pPrint)
+import Util (eventInfoToString, histo)
 
-histo :: [String] -> [(String, Int)]
-histo = mapMaybe count . group . sort
-  where
-    count ys@(x : xs) = Just (x, length ys)
-    count [] = Nothing
-
-eventInfoToString :: EventInfo -> String
-eventInfoToString info =
-  case info of
-    EventBlock {} -> "EventBlock"
-    UnknownEvent {} -> "UnknownEvent"
-    Startup {} -> "Startup"
-    Shutdown {} -> "Shutdown"
-    CreateThread {} -> "CreateThread"
-    RunThread {} -> "RunThread"
-    StopThread {} -> "StopThread"
-    ThreadRunnable {} -> "ThreadRunnable"
-    MigrateThread {} -> "MigrateThread"
-    WakeupThread {} -> "WakeupThread"
-    ThreadLabel {} -> "ThreadLabel"
-    CreateSparkThread {} -> "CreateSparkThread"
-    SparkCounters {} -> "SparkCounters"
-    SparkCreate {} -> "SparkCreate"
-    SparkDud {} -> "SparkDud"
-    SparkOverflow {} -> "SparkOverflow"
-    SparkRun {} -> "SparkRun"
-    SparkSteal {} -> "SparkSteal"
-    SparkFizzle {} -> "SparkFizzle"
-    SparkGC {} -> "SparkGC"
-    TaskCreate {} -> "TaskCreate"
-    TaskMigrate {} -> "TaskMigrate"
-    TaskDelete {} -> "TaskDelete"
-    RequestSeqGC {} -> "RequestSeqGC"
-    RequestParGC {} -> "RequestParGC"
-    StartGC {} -> "StartGC"
-    GCWork {} -> "GCWork"
-    GCIdle {} -> "GCIdle"
-    GCDone {} -> "GCDone"
-    EndGC {} -> "EndGC"
-    GlobalSyncGC {} -> "GlobalSyncGC"
-    GCStatsGHC {} -> "GCStatsGHC"
-    MemReturn {} -> "MemReturn"
-    HeapAllocated {} -> "HeapAllocated"
-    HeapSize {} -> "HeapSize"
-    BlocksSize {} -> "BlocksSize"
-    HeapLive {} -> "HeapLive"
-    HeapInfoGHC {} -> "HeapInfoGHC"
-    CapCreate {} -> "CapCreate"
-    CapDelete {} -> "CapDelete"
-    CapDisable {} -> "CapDisable"
-    CapEnable {} -> "CapEnable"
-    CapsetCreate {} -> "CapsetCreate"
-    CapsetDelete {} -> "CapsetDelete"
-    CapsetAssignCap {} -> "CapsetAssignCap"
-    CapsetRemoveCap {} -> "CapsetRemoveCap"
-    RtsIdentifier {} -> "RtsIdentifier"
-    ProgramArgs {} -> "ProgramArgs"
-    ProgramEnv {} -> "ProgramEnv"
-    OsProcessPid {} -> "OsProcessPid"
-    OsProcessParentPid {} -> "OsProcessParentPid"
-    WallClockTime {} -> "WallClockTime"
-    Message {} -> "Message"
-    UserMessage {} -> "UserMessage"
-    UserMarker {} -> "UserMarker"
-    Version {} -> "Version"
-    ProgramInvocation {} -> "ProgramInvocation"
-    CreateMachine {} -> "CreateMachine"
-    KillMachine {} -> "KillMachine"
-    CreateProcess {} -> "CreateProcess"
-    KillProcess {} -> "KillProcess"
-    AssignThreadToProcess {} -> "AssignThreadToProcess"
-    EdenStartReceive {} -> "EdenStartReceive"
-    EdenEndReceive {} -> "EdenEndReceive"
-    SendMessage {} -> "SendMessage"
-    ReceiveMessage {} -> "ReceiveMessage"
-    SendReceiveLocalMessage {} -> "SendReceiveLocalMessage"
-    InternString {} -> "InternString"
-    MerStartParConjunction {} -> "MerStartParConjunction"
-    MerEndParConjunction {} -> "MerEndParConjunction"
-    MerEndParConjunct {} -> "MerEndParConjunct"
-    MerCreateSpark {} -> "MerCreateSpark"
-    MerFutureCreate {} -> "MerFutureCreate"
-    MerFutureWaitNosuspend {} -> "MerFutureWaitNosuspend"
-    MerFutureWaitSuspended {} -> "MerFutureWaitSuspended"
-    MerFutureSignal {} -> "MerFutureSignal"
-    MerLookingForGlobalThread {} -> "MerLookingForGlobalThread"
-    MerWorkStealing {} -> "MerWorkStealing"
-    MerLookingForLocalSpark {} -> "MerLookingForLocalSpark"
-    MerReleaseThread {} -> "MerReleaseThread"
-    MerCapSleeping {} -> "MerCapSleeping"
-    MerCallingMain {} -> "MerCallingMain"
-    PerfName {} -> "PerfName"
-    PerfCounter {} -> "PerfCounter"
-    PerfTracepoint {} -> "PerfTracepoint"
-    HeapProfBegin {} -> "HeapProfBegin"
-    HeapProfCostCentre {} -> "HeapProfCostCentre"
-    InfoTableProv {} -> "InfoTableProv"
-    HeapProfSampleBegin {} -> "HeapProfSampleBegin"
-    HeapProfSampleEnd {} -> "HeapProfSampleEnd"
-    HeapBioProfSampleBegin {} -> "HeapBioProfSampleBegin"
-    HeapProfSampleCostCentre {} -> "HeapBioProfSampleCostCentre"
-    HeapProfSampleString {} -> "HeapProfSampleString"
-    ProfSampleCostCentre {} -> "ProfSampleCostCentre"
-    ProfBegin {} -> "ProfBegin"
-    UserBinaryMessage {} -> "UserBinaryMessage"
-    ConcMarkBegin {} -> "ConcMarkBegin"
-    ConcMarkEnd {} -> "ConcMarkEnd"
-    ConcSyncBegin {} -> "ConcSyncBegin"
-    ConcSyncEnd {} -> "ConcSyncEnd"
-    ConcSweepBegin {} -> "ConcSweepBegin"
-    ConcSweepEnd {} -> "ConcSweepEnd"
-    ConcUpdRemSetFlush {} -> "ConcUpdRemSetFlush"
-    NonmovingHeapCensus {} -> "NonmovingHeapCensus"
-    TickyCounterDef {} -> "TickyCounterDef"
-    TickyCounterSample {} -> "TickyCounterSample"
-    TickyBeginSample {} -> "TickyBeginSample"
+data MyState = MyState Double
 
 recordEvent :: IORef Int -> Event -> IO ()
 recordEvent ref ev = do
-  modifyIORef' ref (+1)
+  modifyIORef' ref (+ 1)
   pPrint ev
 
 dump :: IORef Int -> Socket -> IO ()
@@ -203,33 +91,47 @@ dump ref sock = goHeader ""
       bytes'' <- recv sock 1024
       goEvents hdr dec' (bytes' <> bytes'')
 
+myDraw :: IORef Int -> IORef MyState -> R.Render ()
+myDraw ref ref' = do
+  MyState x <- liftIO $ readIORef ref'
+  R.setSourceRGBA 0.16 0.18 0.19 1.0
+  R.setLineWidth (1.5 / 60)
+  R.rectangle x x 50 50
+  R.fill
+  liftIO $ putStrLn "draw called"
+  liftIO $ do
+    n <- readIORef ref
+    print n
+
+tickTock :: Gtk.DrawingArea -> IORef MyState -> IO ()
+tickTock drawingArea ref' = forever $ do
+  threadDelay 50_000
+  modifyIORef' ref' (\(MyState x) -> MyState (x + 1))
+  postGUIASync $
+    #queueDraw drawingArea
+
 main :: IO ()
 main = do
   ref <- newIORef 0
+  ref' <- newIORef (MyState 10)
 
-  _ <- GI.init Nothing
-  mainWindow <- new GI.Window [ #type := GI.WindowTypeToplevel ]
-  drawingArea <- new GI.DrawingArea []
+  _ <- Gtk.init Nothing
+  mainWindow <- new Gtk.Window [#type := Gtk.WindowTypeToplevel]
+  drawingArea <- new Gtk.DrawingArea []
   drawingArea `on` #draw $
     renderWithContext $ do
-      R.setSourceRGBA 0.16 0.18 0.19 1.0
-      R.setLineWidth (1.5/60)
-      R.rectangle 10 10 50 50
-      R.fill
-      liftIO $ putStrLn "draw called"
-      liftIO $ do
-        n <- readIORef ref
-        print n
+      myDraw ref ref'
       pure True
   layout <- do
-    vbox <- new GI.Box [#orientation := GI.OrientationVertical, #spacing := 0]
+    vbox <- new Gtk.Box [#orientation := Gtk.OrientationVertical, #spacing := 0]
     #packStart vbox drawingArea True True 0
     pure vbox
   #add mainWindow layout
   #showAll mainWindow
 
+  _ <- forkIO $ tickTock drawingArea ref'
   _ <- forkIO (receiver ref)
-  GI.main
+  Gtk.main
 
 receiver :: IORef Int -> IO ()
 receiver ref =

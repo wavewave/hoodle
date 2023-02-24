a960bf06754f27908a149e17713a76f0ec96abad
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-24 10:41:40 -0800

    use TVar. flush queue with printing diff histo

--- log/app/logcat/Main.hs
+++ log/app/logcat/Main.hs
@@ -7,15 +7,17 @@
 module Main where
 
 import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.STM (TVar, atomically, modifyTVar', newTVarIO, readTVar)
 import qualified Control.Exception as E
-import Control.Lens (makeLenses, (%~))
+import Control.Lens (makeLenses, (%~), (.~), (^.))
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import Data.Foldable (toList)
 import Data.GI.Base (AttrOp ((:=)), new, on)
 import Data.GI.Gtk.Threading (postGUIASync)
-import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
+-- import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
 import Data.Maybe (fromMaybe)
 import Data.Sequence (Seq, (|>))
 import qualified Data.Sequence as Seq (empty)
@@ -41,24 +43,36 @@ import Network.Socket
 import Network.Socket.ByteString (recv)
 import System.IO (hFlush, stdout)
 import Text.Pretty.Simple (pPrint)
-
--- import Util (eventInfoToString, histo)
+import Util (eventInfoToString, histo)
 
 data LogcatState = LogcatState
-  { _logcatEventStore :: Seq Event
+  { _logcatEventStore :: Seq Event,
+    -- TODO: Queue should be a local state, not a global state, considering STM overhead.
+    _logcatEventQueue :: Seq Event,
+    _logcatEventHisto :: [(String, Int)]
   }
 
 makeLenses ''LogcatState
 
 emptyLogcatState :: LogcatState
-emptyLogcatState = LogcatState Seq.empty
+emptyLogcatState = LogcatState Seq.empty Seq.empty []
 
-recordEvent :: IORef LogcatState -> Event -> IO ()
-recordEvent sref ev = do
-  modifyIORef' sref (logcatEventStore %~ (|> ev))
-  pPrint ev
+recordEvent :: TVar LogcatState -> Event -> IO ()
+recordEvent sref ev =
+  atomically $ modifyTVar' sref (logcatEventQueue %~ (|> ev))
 
-dump :: IORef LogcatState -> Socket -> IO ()
+flushEventQueue :: TVar LogcatState -> IO ()
+flushEventQueue sref = do
+  h <- atomically $ do
+    queue <- (^. logcatEventQueue) <$> readTVar sref
+    let h = histo $ fmap (eventInfoToString . evSpec) $ toList queue
+    modifyTVar' sref $
+      (logcatEventStore %~ (<> queue))
+        . (logcatEventQueue .~ Seq.empty)
+    pure h
+  print h
+
+dump :: TVar LogcatState -> Socket -> IO ()
 dump sref sock = goHeader ""
   where
     goHeader bs0 = do
@@ -98,24 +112,27 @@ dump sref sock = goHeader ""
       bytes'' <- recv sock 1024
       goEvents hdr dec' (bytes' <> bytes'')
 
-myDraw :: IORef LogcatState -> R.Render ()
+myDraw :: TVar LogcatState -> R.Render ()
 myDraw sref = do
-  LogcatState evs <- liftIO $ readIORef sref
+  evs <-
+    liftIO $
+      (^. logcatEventStore) <$> atomically (readTVar sref)
   let x = fromIntegral (length evs) / 1000.0
   R.setSourceRGBA 0.16 0.18 0.19 1.0
   R.setLineWidth (1.5 / 60)
   R.rectangle x 10 50 50
   R.fill
 
-tickTock :: Gtk.DrawingArea -> IO ()
-tickTock drawingArea = forever $ do
+tickTock :: Gtk.DrawingArea -> TVar LogcatState -> IO ()
+tickTock drawingArea sref = forever $ do
   threadDelay 1_000_000
+  flushEventQueue sref
   postGUIASync $
     #queueDraw drawingArea
 
 main :: IO ()
 main = do
-  sref <- newIORef emptyLogcatState
+  sref <- newTVarIO emptyLogcatState
 
   _ <- Gtk.init Nothing
   mainWindow <- new Gtk.Window [#type := Gtk.WindowTypeToplevel]
@@ -131,11 +148,11 @@ main = do
   #add mainWindow layout
   #showAll mainWindow
 
-  _ <- forkIO $ tickTock drawingArea
-  _ <- forkIO (receiver sref)
+  _ <- forkIO $ tickTock drawingArea sref
+  _ <- forkIO $ receiver sref
   Gtk.main
 
-receiver :: IORef LogcatState -> IO ()
+receiver :: TVar LogcatState -> IO ()
 receiver sref =
   withSocketsDo $ do
     let file = "/tmp/eventlog.sock"

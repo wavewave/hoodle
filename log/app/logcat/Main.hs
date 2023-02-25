12389cd329c62facb86c56beecd07f3a92ec2217
Ian-Woo Kim <ianwookim@gmail.com>
2023-02-24 20:11:54 -0800

    Show eventlog timeline (#150)
    Each eventlog event is marked in the timeline graph, where x-axis is time and y-axis denotes event types.
    The logcat canvas is double-buffered so to avoid re-rendering every draw event call.
    
    * show eventlog timeline
    * use double buffer.
    * time grid.
    * time axis tick labels

--- log/app/logcat/Main.hs
+++ log/app/logcat/Main.hs
@@ -14,6 +14,7 @@ import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import Data.Fixed (Fixed (MkFixed), Nano)
 import Data.Foldable (for_, toList)
 import Data.GI.Base (AttrOp ((:=)), new, on)
 import Data.GI.Gtk.Threading (postGUIASync)
@@ -64,8 +65,8 @@ recordEvent :: TVar LogcatState -> Event -> IO ()
 recordEvent sref ev =
   atomically $ modifyTVar' sref (logcatEventQueue %~ (|> ev))
 
-flushEventQueue :: TVar LogcatState -> IO ()
-flushEventQueue sref =
+flushEventQueue :: R.Surface -> TVar LogcatState -> IO ()
+flushEventQueue sfc sref = do
   atomically $ do
     s <- readTVar sref
     let queue = s ^. logcatEventQueue
@@ -77,6 +78,8 @@ flushEventQueue sref =
       (logcatEventStore %~ (<> queue))
         . (logcatEventQueue .~ Seq.empty)
         . (logcatEventHisto .~ hist')
+  R.renderWith sfc $ do
+    drawLogcatState sref
 
 dump :: TVar LogcatState -> Socket -> IO ()
 dump sref sock = goHeader ""
@@ -118,71 +121,136 @@ dump sref sock = goHeader ""
       bytes'' <- recv sock 1024
       goEvents hdr dec' (bytes' <> bytes'')
 
-{-
--- NOT IMPLEMENTED YET
+canvasWidth :: Double
+canvasWidth = 1440
+
+canvasHeight :: Double
+canvasHeight = 768
+
+secToPixel :: Nano -> Double
+secToPixel sec =
+  realToFrac sec * 10.0 + 10
+
+pixelToSec :: Double -> Nano
+pixelToSec px =
+  realToFrac ((px - 10.0) / 10.0)
+
+drawEventMark :: Event -> R.Render ()
+drawEventMark ev = do
+  let sec = MkFixed (fromIntegral (evTime ev)) :: Nano
+      --  x :: Double
+      x = secToPixel sec -- realToFrac sec * 10.0 + 10
+      evname = eventInfoToString (evSpec ev)
+      tag = fromMaybe 0 (L.lookup evname eventInfoEnumMap)
+      y = fromIntegral tag * 3.0
+  R.moveTo x y
+  R.lineTo x (y + 2)
+  R.stroke
+
+drawTimeGrid :: R.Render ()
+drawTimeGrid = do
+  let tmax = pixelToSec canvasWidth
+      ts = [0, 1 .. tmax]
+      lblTs = [0, 10 .. tmax]
+  R.setSourceRGBA 0 0 1 0.5
+  R.setLineWidth 0.1
+  R.setLineCap R.LineCapRound
+  R.setLineJoin R.LineJoinRound
+  for_ ts $ \t -> do
+    let x = secToPixel t
+    R.moveTo x 0
+    R.lineTo x 150
+    R.stroke
+  R.setSourceRGBA 0 0 1 0.8
+  R.setFontSize 8
+  for_ lblTs $ \t -> do
+    R.moveTo (secToPixel t) 10
+    R.textPath (show (floor t :: Int) <> " s")
+    R.stroke
+
 drawTimeline :: Seq Event -> R.Render ()
 drawTimeline evs = do
-  let x = fromIntegral (length evs) / 1000.0
+  drawTimeGrid
   R.setSourceRGBA 0.16 0.18 0.19 1.0
-  R.setLineWidth (1.5 / 60)
-  R.rectangle x 10 50 50
-  R.fill
--}
+  R.setLineWidth 0.3
+  R.setLineCap R.LineCapRound
+  R.setLineJoin R.LineJoinRound
+  for_ evs $ \ev ->
+    drawEventMark ev
 
-drawHistBar :: (String, Int) -> R.Render ()
-drawHistBar (ev, value) = do
-  let xoffset = 10
-      yoffset = 0
-      tag = fromMaybe 0 (L.lookup ev eventInfoEnumMap)
+drawHistBar :: (Double, Double) -> (String, Int) -> R.Render ()
+drawHistBar (xoffset, yoffset) (ev, value) = do
+  let tag = fromMaybe 0 (L.lookup ev eventInfoEnumMap)
   R.setSourceRGBA 0.16 0.18 0.19 1.0
-  R.setLineWidth (1.5 / 60)
+  R.setLineWidth 1.0
   let y = yoffset + 10.0 * fromIntegral tag
       w = fromIntegral value / 100.0
   R.moveTo xoffset (y + 10.0)
   R.setFontSize 8.0
   R.textPath ev
+  R.fill
   R.rectangle (xoffset + 100) (y + 2) w 8
   R.fill
   R.moveTo (xoffset + 104 + w) (y + 10.0)
   R.textPath (show value)
+  R.fill
 
 drawLogcatState :: TVar LogcatState -> R.Render ()
 drawLogcatState sref = do
+  R.setSourceRGB 1 1 1
+  R.rectangle 0 0 canvasWidth canvasHeight
+  R.fill
   s <- liftIO $ atomically $ readTVar sref
-  let -- evs = s ^. logcatEventStore
+  let evs = s ^. logcatEventStore
       hist = s ^. logcatEventHisto
-  -- drawTimeline evs
+  let xoffset = 10
+      yoffset = 100
+  drawTimeline evs
+  R.setSourceRGBA 0 0 1 0.8
+  R.setLineWidth 1
+  R.moveTo 0 150
+  R.lineTo canvasWidth 150
+  R.stroke
   for_ (Map.toAscList hist) $ \(ev, value) ->
-    drawHistBar (ev, value)
+    drawHistBar (xoffset, yoffset) (ev, value)
 
-tickTock :: Gtk.DrawingArea -> TVar LogcatState -> IO ()
-tickTock drawingArea sref = forever $ do
+flushDoubleBuffer :: R.Surface -> R.Render ()
+flushDoubleBuffer sfc = do
+  R.setSourceSurface sfc 0 0
+  R.setOperator R.OperatorSource
+  R.paint
+
+tickTock :: Gtk.DrawingArea -> R.Surface -> TVar LogcatState -> IO ()
+tickTock drawingArea sfc sref = forever $ do
   threadDelay 1_000_000
-  flushEventQueue sref
+  flushEventQueue sfc sref
   postGUIASync $
     #queueDraw drawingArea
 
 main :: IO ()
 main = do
   sref <- newTVarIO emptyLogcatState
-
   _ <- Gtk.init Nothing
+  -- NOTE: this should be closed with surfaceDestroy
+  sfc <- R.createImageSurface R.FormatARGB32 (floor canvasWidth) (floor canvasHeight)
   mainWindow <- new Gtk.Window [#type := Gtk.WindowTypeToplevel]
   drawingArea <- new Gtk.DrawingArea []
   _ <- drawingArea `on` #draw $
     renderWithContext $ do
-      drawLogcatState sref
+      flushDoubleBuffer sfc
       pure True
   layout <- do
     vbox <- new Gtk.Box [#orientation := Gtk.OrientationVertical, #spacing := 0]
     #packStart vbox drawingArea True True 0
     pure vbox
   #add mainWindow layout
+  #setDefaultSize mainWindow (floor canvasWidth) (floor canvasHeight)
   #showAll mainWindow
 
-  _ <- forkIO $ tickTock drawingArea sref
+  _ <- forkIO $ tickTock drawingArea sfc sref
   _ <- forkIO $ receiver sref
   Gtk.main
+  R.surfaceFinish sfc
 
 receiver :: TVar LogcatState -> IO ()
 receiver sref =

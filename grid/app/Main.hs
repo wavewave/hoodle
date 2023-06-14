9b55de6657b167a34d8909afc3224a9efe5ed2fc
Ian-Woo Kim <ianwookim@gmail.com>
2023-06-14 12:10:22 -0700

    multi-line text

--- grid/app/Main.hs
+++ grid/app/Main.hs
@@ -5,6 +5,8 @@ module Main where
 
 import Data.Foldable (for_)
 import Data.GI.Base (AttrOp ((:=)), new, on)
+import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Traversable (for)
 import GI.Cairo.Render qualified as R
 import GI.Cairo.Render.Connector as RC
@@ -12,25 +14,46 @@ import GI.Gtk qualified as Gtk
 import GI.Pango qualified as P
 import GI.PangoCairo qualified as PC
 
+loremIpsum :: Text
+loremIpsum =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n\
+  \sed do eiusmod tempor incididunt ut labore et dolore magn\n\
+  \a aliqua. Ut enim ad minim veniam, quis nostrud exercitat\n\
+  \ion ullamco laboris nisi ut aliquip ex ea commodo consequ\n\
+  \at. Duis aute irure dolor in reprehenderit in voluptate v\n\
+  \elit esse cillum dolore eu fugiat nulla pariatur. Excepte\n\
+  \ur sint occaecat cupidatat non proident, sunt in culpa qu\n\
+  \i officia deserunt mollit anim id est laborum."
+
 xs :: [Double]
 xs = [0, 128 .. 1024]
 
 ys :: [Double]
 ys = [0, 128 .. 1024]
 
+drawTextLine :: (P.Context, P.FontDescription) -> (Double, Double) -> Text -> R.Render ()
+drawTextLine (pctxt, desc) (x, y) msg = do
+  layout :: P.Layout <- P.layoutNew pctxt
+  #setSize desc (10 * P.SCALE)
+  #setFontDescription layout (Just desc)
+  #setText layout msg (-1)
+  R.moveTo x y
+  ctxt <- RC.getContext
+  PC.showLayout ctxt layout
+
+drawTextMultiline ::
+  (P.Context, P.FontDescription) ->
+  (Double, Double) ->
+  [Text] ->
+  R.Render ()
+drawTextMultiline (ctxt, desc) (x, y) msgs = do
+  for_ (zip [y, y + 12 .. ] msgs) $ \(y', msg) ->
+    drawTextLine (ctxt, desc) (x, y') msg
+
 myText :: (P.Context, P.FontDescription, P.FontDescription) -> R.Render ()
 myText (pangoCtxt, descSans, descMono) = do
-    mkText descSans (120, 120) "Grid cache testing"
-    mkText descMono (120, 240) "Grid cache testing"
-  where
-    mkText desc (x, y) msg = do
-      layout :: P.Layout <- P.layoutNew pangoCtxt
-      #setSize desc (10 * P.SCALE)
-      #setFontDescription layout (Just desc)
-      #setText layout msg (-1)
-      R.moveTo x y
-      ctxt <- RC.getContext
-      PC.showLayout ctxt layout
+  drawTextMultiline (pangoCtxt, descSans) (120, 120) (T.lines loremIpsum)
+  drawTextMultiline (pangoCtxt, descMono) (120, 240) (T.lines loremIpsum)
 
 myDraw :: (P.Context, P.FontDescription, P.FontDescription) -> R.Render ()
 myDraw (pangoCtxt, descSans, descMono) = do
